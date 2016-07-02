73586b842ad42c2a05d828599161951ebb08d0f6
Ying Chen <chying@google.com>
2016-07-01 17:58:57 -0700

    Download system image builds before each step
    - The purpose is to save disk space
    - Always clean up system image directory and only download before test step
    
    Change-Id: If01f9435a47ccab1d4bdbea7f6a94ed72217e7b4

--- emu_test/utils/download_unzip_image.py
+++ emu_test/utils/download_unzip_image.py
@@ -10,6 +10,7 @@ parser.add_argument('--file', dest='remote_file_list', action='store',
                     help='string contains a list of remote files separated by comma')
 parser.add_argument('--build-dir', action='store',
                     help='location of build directory')
+parser.add_argument('--clean-system-image-dir', action='store_true', help='clean up system image directory')
 
 args = parser.parse_args()
 
@@ -91,6 +92,10 @@ def unzip_addon_dir(file_name, dst_dir):
 
 def download_and_unzip():
   clean_emu_proc()
+  if args.clean_system_image_dir:
+    image_dir = os.path.join(os.environ['ANDROID_SDK_ROOT'], 'system-images')
+    print 'Remove system image directory: ', image_dir
+    verbose_call(['rm', '-rf', image_dir])
   file_list = args.remote_file_list.split(',')
   dst_dir = get_dst_dir(file_list[0])
 
