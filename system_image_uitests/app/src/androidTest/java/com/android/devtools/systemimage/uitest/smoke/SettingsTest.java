a21dba11d2d2c94641f6ac31a9fa1c3488eb6af8
David <dyu@google.com>
2017-02-06 16:11:07 -0800

    Add system image UI test to verify Maps location requires permission access.
    This test works on API 23 or greater, with google APIs only.
    
    Test: manual
    Change-Id: Iec7a830c5c16b20808ee3903a57c29249ff5f3da

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/SettingsTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/SettingsTest.java
@@ -128,54 +128,14 @@ public class SettingsTest {
     public void testPhonePermissions() throws Exception {
         Instrumentation instrumentation = testFramework.getInstrumentation();
         final UiDevice device = UiDevice.getInstance(instrumentation);
+        final String app = "Phone";
 
         if (testFramework.getApi() >= 23) {
             SettingsUtil.openItem(instrumentation, "Apps");
-            device.findObject(new UiSelector().resourceId(Res.SETTINGS_ADVANCED_OPTION_RES))
-                    .clickAndWaitForNewWindow();
-            device.findObject(new UiSelector().text("App permissions"))
-                    .clickAndWaitForNewWindow();
-
-            UiScrollable appPermissionsList = new UiScrollable(
-                    new UiSelector().resourceId(Res.ANDROID_CONTENT_RES));
-            assertTrue(appPermissionsList.exists());
-            appPermissionsList.setAsVerticalList();
-            appPermissionsList.scrollIntoView(new UiSelector().text("Phone"));
-            device.findObject(new UiSelector().text("Phone")).click();
-
-            UiScrollable phonePermissionsList = new UiScrollable(
-                    new UiSelector().resourceId(Res.ANDROID_CONTENT_RES));
-            assertTrue(phonePermissionsList.exists());
-            phonePermissionsList.setAsVerticalList();
-            phonePermissionsList.scrollIntoView(new UiSelector().text("Phone"));
-
-            UiObject phoneToggle = device.findObject(new UiSelector().text("Phone"));
-            phoneToggle.click();
-
-            final UiObject denyButton;
-            if (testFramework.getApi() == 23) {
-                denyButton = device.findObject(new UiSelector().text("Deny"));
-            } else {
-                denyButton = device.findObject(new UiSelector().text("DENY ANYWAY"));
-            }
-
-
-            boolean isSuccess =
-                    new Wait().until(new Wait.ExpectedCondition() {
-                        @Override
-                        public boolean isTrue() throws Exception {
-                            return denyButton.exists();
-                        }
-                    });
-
-            if (!isSuccess) {
-                phoneToggle.clickAndWaitForNewWindow();
-            }
-
-            denyButton.click();
+            SettingsUtil.setAppPermissions(instrumentation, app, app, false);
 
             device.pressHome();
-            device.findObject(new UiSelector().text("Phone")).click();
+            device.findObject(new UiSelector().text(app)).click();
             device.findObject(new UiSelector().description("dial pad")).click();
             for (int i=0; i<3; i++) {
                 device.findObject(new UiSelector().text("JKL")).click();
@@ -193,22 +153,100 @@ public class SettingsTest {
                         }
                     }));
 
-            SettingsUtil.openItem(instrumentation, "Apps");
-            device.findObject(new UiSelector().resourceId(Res.SETTINGS_ADVANCED_OPTION_RES))
+            SettingsUtil.setAppPermissions(instrumentation, app, app, true);
+            device.pressHome();
+        }
+    }
+
+    /**
+     * Verifies that access must be confirmed if Maps location permissions are disabled.
+     * <p>
+     * This is run to qualify releases. Please involve the test team in substantial changes.
+     * <p>
+     * TR ID: C14578843
+     * <p>
+     *   <pre>
+     *  1. Start the Emulator.
+     *  2. Open Settings > Apps
+     *  3. Click on the gear icon and select App permissions.
+     *  4. Click on Location from App permissions screen.
+     *  5. Disable Maps.
+     *  6. Launch Maps app.
+     *  7. Click on the locator icon.
+     *   Verify:
+     *   Dialog asking "Allow Maps to access this device's location?"
+     *   </pre>
+     * <p>
+     * The test works on API 23 and greater, with google APIs only.
+     * No gear menu and app permissions for APIs under 23, and no maps without google APIs.
+     */
+    @Test
+    @TestInfo(id = "14578843")
+    public void testMapPermissions() throws Exception {
+        Instrumentation instrumentation = testFramework.getInstrumentation();
+        final UiDevice device = UiDevice.getInstance(instrumentation);
+        final String appType = "Location";
+        final String appName = "Maps";
+
+        if (!testFramework.isGoogleApiImage()) {
+            return;
+        }
+
+        if (testFramework.getApi() >= 23) {
+            SettingsUtil.setAppPermissions(instrumentation, appType, appName, false);
+
+            device.pressHome();
+            device.findObject(new UiSelector().description("Apps"))
                     .clickAndWaitForNewWindow();
-            device.findObject(new UiSelector().text("App permissions"))
+            device.findObject(new UiSelector().text(appName))
                     .clickAndWaitForNewWindow();
 
-            assertTrue(appPermissionsList.exists());
-            appPermissionsList.setAsVerticalList();
-            appPermissionsList.scrollIntoView(new UiSelector().text("Phone"));
-            device.findObject(new UiSelector().text("Phone")).click();
+            final UiObject acceptAndContinueButton;
+            if (testFramework.getApi() == 23) {
+                acceptAndContinueButton = device.findObject(
+                        new UiSelector().text("Accept & continue"));
+            } else {
+                acceptAndContinueButton = device.findObject(
+                        new UiSelector().text("ACCEPT & CONTINUE"));
+            }
+            if (acceptAndContinueButton.exists()) {
+                acceptAndContinueButton.clickAndWaitForNewWindow();
+            }
 
-            assertTrue(phonePermissionsList.exists());
-            phonePermissionsList.setAsVerticalList();
-            phonePermissionsList.scrollIntoView(new UiSelector().text("Phone"));
+            final UiObject skipButton;
+            if (testFramework.getApi() == 23) {
+                skipButton = device.findObject(new UiSelector().text("Skip"));
+            } else {
+                skipButton = device.findObject(new UiSelector().text("SKIP"));
+            }
+            if (skipButton.exists()) {
+                skipButton.clickAndWaitForNewWindow();
+            }
 
-            phoneToggle.click();
+            final UiObject gotItButton;
+            if (testFramework.getApi() == 23) {
+                gotItButton = device.findObject(new UiSelector().text("Got it"));
+            } else {
+                gotItButton = device.findObject(new UiSelector().text("GOT IT"));
+            }
+            if (gotItButton.exists()) {
+                gotItButton.clickAndWaitForNewWindow();
+            }
+
+            device.findObject(new UiSelector().description("Move to your location"))
+                    .clickAndWaitForNewWindow();
+
+            assertTrue("Did not prompt for lack of Maps permission.",
+                    new Wait().until(new Wait.ExpectedCondition() {
+                        @Override
+                        public boolean isTrue() throws Exception {
+                            return device.findObject(new UiSelector()
+                                    .text("Allow Maps to access this device's location?")).exists();
+                        }
+                    }));
+
+            SettingsUtil.setAppPermissions(instrumentation, appType, appName, true);
+
             device.pressHome();
         }
     }
