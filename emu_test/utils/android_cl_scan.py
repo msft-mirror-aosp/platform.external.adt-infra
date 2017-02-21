2773064e67b05179ae7ec18a1abf2efd7cbc64c8
cpilch <cpilch@google.com>
2017-02-21 14:10:57 -0800

    Updates code to fix spelling mistake of mac as windows.
    Change-Id: I1887a2c879c28c357d0cdb57c1ca7cbf1e747421

--- emu_test/utils/android_cl_scan.py
+++ emu_test/utils/android_cl_scan.py
@@ -116,7 +116,7 @@ def generate_git_information(poller):
     return 'git_master', 'sdk_gphone_x86-sdk_addon'
   elif 'emulator_2.3_linux_poller' in poller or 'emulator_2.3_windows_poller' in poller:
     return 'aosp-emu-2.3-release', 'sdk_tools_linux'
-  elif 'emulator_2.3_windows_poller' in poller:
+  elif 'emulator_2.3_mac_poller' in poller:
     return 'aosp-emu-2.3-release', 'sdk_tools_mac'
   else:
     raise NotImplementedError('The passed in poller: %s, does not have a '
