78e15918e7e4394174fa74ceb1ce9d36ffca0676
cpilch <cpilch@google.com>
2017-04-07 10:16:06 -0700

    Updates reset git command to proper branch
    Every since the switch to the new emu-master-dev branch, this reset has
    been resetting improperly.  We need to fix this so we dont keep
    recycling back to master branch.
    
    Change-Id: Id2b7e083b6d35279cb1af1748a1f46d7afe8751b

--- build/scripts/slave/annotated_run.py
+++ build/scripts/slave/annotated_run.py
@@ -182,7 +182,7 @@ def update_scripts():
   git_cmd = 'git.bat' if os.name == "nt" else 'git'
   with stream.step('update_scripts') as s:
     fetch_cmd = [git_cmd, 'fetch', '--all']
-    reset_cmd = [git_cmd, 'reset', '--hard', 'origin/master']
+    reset_cmd = [git_cmd, 'reset', '--hard', 'origin/emu-master-dev']
     if subprocess.call(fetch_cmd) != 0 or subprocess.call(reset_cmd) != 0:
       s.step_text('git update source failed!')
       s.step_warnings()
