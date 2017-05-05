38cb2aa3a85c07026d5c1e6168499c0dc048bd77
David <dyu@google.com>
2017-05-05 10:35:19 -0700

    Add test to verify parental controls work in play store.
    TEST: local
    Change-Id: Iac8de3d0d6c6b061dc25651e86c5156fe6ce034c

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/PlayStoreTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/PlayStoreTest.java
@@ -35,6 +35,8 @@ import org.junit.Test;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -264,6 +266,145 @@ public class PlayStoreTest {
     }
 
     /**
+     * Verify that Google Play search can be limited through parental controls
+     * <p>
+     * TR ID: C14605497
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator and launch home screen.
+     *   2. Open Apps.
+     *   3. Confirm that Play Store is present, then launch.
+     *   4. Search for adult app in Play Store, successfully.
+     *   5. Open Play Store settings.
+     *   6. Turn on Parental Controls.
+     *   7. Select restrictions for Apps and Games.
+     *   8. Set and confirm a content PIN.
+     *   9. Set controls to Everone 10+.
+     *   10. Search for adult app in Play Store, unsuccessfully.
+     *   11. Search for family app in Play Store, successfully.
+     *   12. Turn off Parental Controls.
+     *   Verify:
+     *      1. Confirm that search returns adult version of app without parental controls set.
+     *      2. Confirm that search does not return adult version of app with parental controls set.
+     *      3. Confirm that search does return family version of app with parental controls set.
+     *   </pre>
+     */
+    @Ignore("Testing play store requires google login that may trigger 2-auth factor. Test to be initiated manually by tester.")
+    @Test
+    @TestInfo(id = "14605497")
+    public void testParentalControls() throws Exception {
+        Instrumentation instrumentation = testFramework.getInstrumentation();
+        final UiDevice device = UiDevice.getInstance(instrumentation);
+        final String application = "Truth or Dare";
+
+        if (testFramework.getApi() >= 24 && testFramework.isGoogleApiImage()) {
+            device.pressHome();
+            device.findObject(new UiSelector().description("Apps")).clickAndWaitForNewWindow();
+
+            boolean playStoreInstalled = PlayStoreUtil.isPlayStoreInstalled(instrumentation);
+
+            if (playStoreInstalled) {
+                PlayStoreUtil.searchGooglePlay(instrumentation, application);
+                assertTrue("Adult application not found in search.",  new Wait().until(new Wait.ExpectedCondition() {
+                    @Override
+                    public boolean isTrue() throws Exception {
+                        return findInGooglePlay(device, "App: "+ application + " Adults");
+                    }
+                }));
+
+                openParentalControls(device);
+
+                boolean controlsOff = new Wait().until(new Wait.ExpectedCondition() {
+                    @Override
+                    public boolean isTrue() throws UiObjectNotFoundException {
+                        return device.findObject(new UiSelector().text("Parental controls are off")).exists();
+                    }
+                });
+
+                if (controlsOff) {
+                    device.findObject(new UiSelector().text("Parental controls are off")).clickAndWaitForNewWindow();
+                    setParentalControlPin(device, "1111");
+                }
+
+                device.findObject(new UiSelector().text("Apps & games"))
+                        .waitForExists(TimeUnit.SECONDS.toMillis(3));
+                device.findObject(new UiSelector().text("Apps & games"))
+                        .clickAndWaitForNewWindow();
+
+                device.findObject(new UiSelector().text("Everyone 10+"))
+                        .waitForExists(TimeUnit.SECONDS.toMillis(3));
+                device.findObject(new UiSelector().text("Everyone 10+"))
+                        .clickAndWaitForNewWindow();
+
+                device.findObject(new UiSelector().text("OK"))
+                        .waitForExists(TimeUnit.SECONDS.toMillis(3));
+                device.findObject(new UiSelector().text("OK"))
+                        .clickAndWaitForNewWindow();
+
+                device.findObject(new UiSelector().text("SAVE"))
+                        .waitForExists(TimeUnit.SECONDS.toMillis(3));
+                device.findObject(new UiSelector().text("SAVE"))
+                        .clickAndWaitForNewWindow();
+
+                device.pressHome();
+
+                PlayStoreUtil.searchGooglePlay(instrumentation, application);
+                assertTrue("Adult application found in search.",  new Wait().until(new Wait.ExpectedCondition() {
+                    @Override
+                    public boolean isTrue() throws Exception {
+                        return !findInGooglePlay(device, "App: "+ application + " Adults");
+                    }
+                }));
+
+                assertTrue("Family application not found in search.",  new Wait().until(new Wait.ExpectedCondition() {
+                    @Override
+                    public boolean isTrue() throws Exception {
+                        return findInGooglePlay(device, "App: "+ application + " Kids");
+                    }
+                }));
+
+                openParentalControls(device);
+                new Wait().until(new Wait.ExpectedCondition() {
+                    @Override
+                    public boolean isTrue() throws UiObjectNotFoundException {
+                        return device.findObject(new UiSelector().text("Apps & games")).exists();
+                    }
+                });
+                device.findObject(new UiSelector().text("Apps & games")).clickAndWaitForNewWindow();
+                setParentalControlPin(device, "1111");
+
+                device.findObject(new UiSelector().text("Allow all, including unrated"))
+                        .waitForExists(TimeUnit.SECONDS.toMillis(3));
+                device.findObject(new UiSelector().text("Allow all, including unrated"))
+                        .clickAndWaitForNewWindow();
+
+                device.findObject(new UiSelector().text("OK"))
+                        .waitForExists(TimeUnit.SECONDS.toMillis(3));
+                device.findObject(new UiSelector().text("OK"))
+                        .clickAndWaitForNewWindow();
+
+                device.findObject(new UiSelector().text("SAVE"))
+                        .waitForExists(TimeUnit.SECONDS.toMillis(3));
+                device.findObject(new UiSelector().text("SAVE"))
+                        .clickAndWaitForNewWindow();
+
+                new Wait().until(new Wait.ExpectedCondition() {
+                    @Override
+                    public boolean isTrue() throws UiObjectNotFoundException {
+                        return device.findObject(new UiSelector().text("Parental controls are on")).exists();
+                    }
+                });
+
+                device.findObject(new UiSelector().text("Parental controls are on")).clickAndWaitForNewWindow();
+
+                PlayStoreUtil.resetPlayStore(instrumentation);
+                device.pressHome();
+            }
+        }
+    }
+
+    /**
      * Selects an application listed in Play Store.
      */
     private static void selectFromGooglePlay(UiDevice testDevice, String appDescription) throws Exception {
@@ -292,5 +433,67 @@ public class PlayStoreTest {
                         .description(application)).exists();
             }
         });
+    }
+
+    /**
+     * Opens the Parental Controls menu
+     */
+    private static void openParentalControls(UiDevice testDevice) throws Exception {
+        final UiDevice device = testDevice;
+        device.findObject(new UiSelector().description("Back")).clickAndWaitForNewWindow();
+        device.findObject(new UiSelector().description("Show navigation drawer"))
+                .waitForExists(TimeUnit.SECONDS.toMillis(3));
+        device.findObject(new UiSelector().description("Show navigation drawer"))
+                .clickAndWaitForNewWindow();
+
+        final UiScrollable scrollable = new UiScrollable(new UiSelector().scrollable(true));
+
+        new Wait().until(new Wait.ExpectedCondition() {
+            @Override
+            public boolean isTrue() throws UiObjectNotFoundException {
+                scrollable.scrollIntoView(new UiSelector().text("Settings"));
+                return scrollable.getChild(new UiSelector().text("Settings")).exists();
+            }
+        });
+
+        scrollable.getChild(new UiSelector().text("Settings")).clickAndWaitForNewWindow();
+
+        new Wait().until(new Wait.ExpectedCondition() {
+            @Override
+            public boolean isTrue() throws UiObjectNotFoundException {
+                scrollable.scrollIntoView(new UiSelector().text("Parental controls"));
+                return device.findObject(new UiSelector().text("Parental controls")).exists();
+            }
+        });
+        scrollable.getChild(new UiSelector().text("Parental controls")).clickAndWaitForNewWindow();
+    }
+
+    /**
+     * Sets and then confirms a parental control pin
+     */
+    private static void setParentalControlPin(UiDevice testDevice, String pin) throws Exception {
+        final UiDevice device = testDevice;
+
+        new Wait().until(new Wait.ExpectedCondition() {
+            @Override
+            public boolean isTrue() throws UiObjectNotFoundException {
+                return device.findObject(new UiSelector().text("Type PIN")).exists();
+            }
+        });
+
+        device.findObject(new UiSelector().text("Type PIN")).setText(pin);
+        device.findObject(new UiSelector().text("OK")).clickAndWaitForNewWindow();
+
+        boolean needsConfirmation = new Wait().until(new Wait.ExpectedCondition() {
+            @Override
+            public boolean isTrue() throws UiObjectNotFoundException {
+                return device.findObject(new UiSelector().text("Type PIN")).exists();
+            }
+        });
+
+        if (needsConfirmation) {
+            device.findObject(new UiSelector().text("Type PIN")).setText(pin);
+            device.findObject(new UiSelector().text("OK")).clickAndWaitForNewWindow();
+        }
     }
 }
