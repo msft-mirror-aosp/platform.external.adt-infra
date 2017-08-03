dbb8e13155f5f7ce4e2f4222c2e6a032046e9696
Harrison Ding <harrisonding@google.com>
2017-08-03 10:10:08 -0700

    Fixed path for uploading data to GCS
    Modified Files:
    - upload_test_stats_to_gcs.py
    	Modified the file path so it doesn't go to /tmp/ in LLDB build
    
    Change-Id: I1d8e5d55932a89b29510f62419466b54b0d67cf4

--- emu_test/utils/upload_test_stats_to_gcs.py
+++ emu_test/utils/upload_test_stats_to_gcs.py
@@ -34,7 +34,7 @@ def upload_to_gcs():
     print 'Run command {}'.format(' '.join(cmd))
     subprocess.check_call(cmd)
   requested_date = datetime.datetime.fromtimestamp(args.timestamp).date()
-  filename = '/tmp/{}_{}'.format(create_date_format(requested_date), args.buildnum)
+  filename = '{}_{}'.format(create_date_format(requested_date), args.buildnum)
   gsutil_path = os.path.join(args.build_dir, 'third_party', 'gsutil', 'gsutil.py')
 
   if os.path.isfile(filename):
@@ -42,7 +42,7 @@ def upload_to_gcs():
       verbose_call(['python', gsutil_path, 'cp', filename, 'gs://console_si_test_results/emu_console_tests/{}/{}'.format(
           get_platform_type(args.platform), filename)])
     if args.test_type == 'system_image_ui':
-      verbose_call(['python', gsutil_path, 'cp', filename, 'gs://console_si_test_results/si_ui_tests/{}/{}'.format(
+      verbose_call(['python', gsutil_path, 'cp', '/tmp/{}'.format(filename), 'gs://console_si_test_results/si_ui_tests/{}/{}'.format(
           get_platform_type(args.platform), filename)])
     verbose_call(['rm', filename])
   else:
