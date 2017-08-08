510ca654769c49c29ee4d9da17bdc0e4c4058348
Harrison Ding <harrisonding@google.com>
2017-08-08 15:27:33 -0700

    Fixed bug where passed in parameter is supposed to be a list
    Modified Files:
    - upload_test_stats_to_gcs.py
    	Changed parameter from separate variables into one list variable
    	for verbose_call
    
    Change-Id: Ia8ac416943a57bf2a0b7bce8d57b20d143744ddf

--- emu_test/utils/upload_test_stats_to_gcs.py
+++ emu_test/utils/upload_test_stats_to_gcs.py
@@ -45,7 +45,7 @@ def upload_to_gcs():
     if args.test_type == 'system_image_ui':
       verbose_call(['python', gsutil_path, 'cp', cp_filename, 'gs://console_si_test_results/si_ui_tests/{}/{}'.format(
           get_platform_type(args.platform), filename)])
-    verbose_call('rm', cp_filename)
+    verbose_call(['rm', cp_filename])
   else:
     print "Uploading to GCS failed due to failure to find file"
 
