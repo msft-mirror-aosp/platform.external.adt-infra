dda1466063d06afaa748e012140606259d8a7ced
Christopher Pilch <cpilch@google.com>
2017-11-16 14:53:33 -0800

    Updates ADB recipe to clean log directory
    Always clean the log (build) directory when starting a ADB test.  This
    is to guarentee we stop running out of space on the devices (many of
    which do not have a large amount of space).
    
    Test:  Ran Locally.
    Change-Id: I375302c3ca7e232ae5027ebbadd4c24919c488d1

--- build/scripts/slave/recipes/adt/adb.py
+++ build/scripts/slave/recipes/adt/adb.py
@@ -44,6 +44,9 @@ def RunSteps(api):
     api.repo.sync('-c', 'system/core')
     api.repo.sync('-c', 'development')
 
+  # Force a clean of the log dir to free space regardless of last run failure condition.
+  api.file.rmcontents("Remove old logs", api.path.join(build_dir, "slave", "ADB", "build"))
+
   # Run adb stree tests
   with api.step.defer_results():
     if not api.platform.is_win:
