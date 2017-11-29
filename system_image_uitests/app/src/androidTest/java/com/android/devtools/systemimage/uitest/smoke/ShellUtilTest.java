ea2faa757d87aaf64b20eb724fccf8c1b5beb7e3
David <dyu@google.com>
2017-11-28 16:56:07 -0800

    Add support for API 25 to create a bug report test.
    Updated system image version of API 25 no longer causes the image to
    freeze and crash when capturing a bug report. Enabling the test to
    spport API 25.
    
    TEST: local
    BUG: none
    Change-Id: I0608d4295950201f30be548de6f4e7510e146976

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/ShellUtilTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/ShellUtilTest.java
@@ -19,7 +19,6 @@ package com.android.devtools.systemimage.uitest.smoke;
 import com.android.devtools.systemimage.uitest.annotations.TestInfo;
 import com.android.devtools.systemimage.uitest.framework.SystemImageTestFramework;
 import com.android.devtools.systemimage.uitest.utils.AppLauncher;
-import com.android.devtools.systemimage.uitest.utils.SettingsUtil;
 import com.android.devtools.systemimage.uitest.utils.ShellUtil;
 import com.android.devtools.systemimage.uitest.utils.DeveloperOptionsManager;
 import com.android.devtools.systemimage.uitest.utils.Wait;
@@ -58,10 +57,6 @@ public class ShellUtilTest {
 
     @Rule
     public Timeout globalTimeout = Timeout.seconds(120);
-
-    private final String BUG_REPORT_DIR = testFramework.getApi() <= 26 ?
-            "/data/data/com.android.shell/files/bugreports" :
-            "/data/user_de/0/com.android.shell/files/bugreports" ;
 
     /**
      * Tests the integrity of Shell utilities.
@@ -123,10 +118,18 @@ public class ShellUtilTest {
     public void createBugReport() throws Exception {
         Instrumentation instrumentation = testFramework.getInstrumentation();
         final UiDevice device = UiDevice.getInstance(instrumentation);
+        final String BUG_REPORT_DIR;
 
-        // Application crashes on API 25. Bug report files not generated due to the crash.
-        if (testFramework.getApi() >= 21 && testFramework.getApi() != 25) {
-            deleteBugReportFiles();
+        if (testFramework.getApi() == 25) {
+            BUG_REPORT_DIR = "/bugreports";
+        } else if (testFramework.getApi() == 27) {
+            BUG_REPORT_DIR = "/data/user_de/0/com.android.shell/files/bugreports";
+        } else {
+            BUG_REPORT_DIR = "/data/data/com.android.shell/files/bugreports";
+        }
+
+        if (testFramework.getApi() >= 21) {
+            deleteBugReportFiles(BUG_REPORT_DIR);
 
             if (!DeveloperOptionsManager.isDeveloperOptionsEnabled(testFramework)) {
                 DeveloperOptionsManager.enableDeveloperOptions(testFramework);
@@ -152,7 +155,7 @@ public class ShellUtilTest {
                             public boolean isTrue() throws Exception {
                                 String result = device.executeShellCommand("ls " + BUG_REPORT_DIR);
                                 Log.d(TAG, "ls result " + result);
-                                boolean success = testFramework.getApi() >= 24 ?
+                                boolean success = testFramework.getApi() >= 25 ?
                                         result.matches("(?s).*bugreport.*\\.png.*")
                                                 && result.matches("(?s).*bugreport.*\\.zip.*") :
                                         result.matches("(?s).*bugreport[-0-9]+\\.png.*")
@@ -163,12 +166,12 @@ public class ShellUtilTest {
                         });
                 Assert.assertTrue("Missing bug report files for png and zip.", gotPngAndZip);
             } finally {
-                deleteBugReportFiles();
+                deleteBugReportFiles(BUG_REPORT_DIR);
             }
         }
     }
 
-    public void deleteBugReportFiles() throws Exception {
+    public void deleteBugReportFiles(String reportDir) throws Exception {
         Log.i(TAG, "Deleting any existing bug report files");
 
         Instrumentation instrumentation = testFramework.getInstrumentation();
@@ -176,14 +179,14 @@ public class ShellUtilTest {
 
         // Delete all png and zip bug reports. Delete the files one at a time, as wildcards
         // don't work.
-        String lsResult = device.executeShellCommand("ls " + BUG_REPORT_DIR);
+        String lsResult = device.executeShellCommand("ls " + reportDir);
         String[] files = lsResult.split("\\s+");
         String filename = testFramework.getApi() >= 24 ? "bugreport.*\\.(png|zip)" :
                 "bugreport[-0-9]+\\.(png|zip)";
 
         for (String file : files) {
             if (file.matches(filename)) {
-                device.executeShellCommand(String.format("rm %s/%s", BUG_REPORT_DIR, file));
+                device.executeShellCommand(String.format("rm %s/%s", reportDir, file));
             }
         }
     }
