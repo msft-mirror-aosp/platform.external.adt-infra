6f9c69bee5c3d72b2a06111bc0ab7961bd89aa76
Yahan Zhou <yahan@google.com>
2018-01-19 15:56:14 -0800

    Fix crossbuild path for master system image
    Master system image gets a wrong emulator path in crossbuid.
    
    Change-Id: Id958cc67d47ea3e9a391fe3c229a01ff2d367162

--- build/scripts/slave/recipes/adt/adt.py
+++ build/scripts/slave/recipes/adt/adt.py
@@ -241,9 +241,9 @@ def RunSteps(api):
         emulator_path = api.path.join(emu_branch, 'emulator', 'emulator')
         #for image from master branch, need tot emulator
         step_data = bootSteps[step]
-        if 'master' in step_data.description:
+        if 'master' in step_data.description and not is_cross_build:
             emulator_path = api.path.join(emu_branch, 'emu-master-dev', 'emulator')
-        if 'aosp' in step_data.description:
+        if 'aosp' in step_data.description and not is_cross_build:
             emulator_path = api.path.join(emu_branch, 'emu-master-dev', 'emulator')
         emu_desc = "sdk emulator" if emu_branch not in emulator_branches else emu_branch
         if not is_cts and not is_ui and not is_console and not is_avd:
