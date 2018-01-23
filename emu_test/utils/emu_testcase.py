80de21e0a024e020ba00b0cfba2dce2b05b53194
Yahan Zhou <yahan@google.com>
2018-01-23 18:38:46 +0000

    Revert "Revert "Add network connection check for P""
    This reverts commit 9a2f20109540ce7cb421eadbd40c10ecd2f4bc2b.
    
    Change-Id: I982c97d055507fb2f0e011fc0f58e9a9bf1bc800

--- emu_test/utils/emu_testcase.py
+++ emu_test/utils/emu_testcase.py
@@ -20,6 +20,7 @@ from emu_error import *
 import emu_test.utils.emu_argparser as emu_argparser
 from subprocess import PIPE, STDOUT
 from collections import namedtuple
+from parse import parse
 
 class AVDConfig(namedtuple('AVDConfig', 'api, alt_version, tag, abi, device, ram, gpu, classic, port, cts, ori')):
     __slots__ = ()
@@ -247,6 +248,24 @@ class EmuBaseTestCase(LoggedTestCase):
         return vars['process'].returncode, vars['output'], vars['err']
 
     def launch_emu_and_wait(self, avd):
+        def check_network_connectivity():
+            ret, output, _ = run_with_timeout(["adb", "shell", "dumpsys", "connectivity"])
+            hasNumNetwork = False
+            dnsSuccess = False
+            for line in output:
+                numNetwork = parse("Active default network:{}", line)
+                if numNetwork:
+                    if numNetwork == 0:
+                        raise ValueError("No network connection")
+                    else:
+                        hasNumNetwork = True
+                if 'PROBE_DNS' in str and 'OK' in line:
+                    dnsSuccess = True
+            if not hasNumNetwork or not dnsSuccess:
+                self.m_logger.error('adb shell dumpsys connectivity retuns:')
+                self.m_logger.error('\n'.join(output))
+                raise Exception('Network check error')
+
         """Launch given avd and wait for boot completion, return boot time"""
         #self.launch_emu(avd)
         self.run_with_timeout(["adb", "kill-server"], 20)
@@ -288,6 +307,12 @@ class EmuBaseTestCase(LoggedTestCase):
             raise TimeoutError(avd, real_time_out)
         self.boot_time = time.time() - start_time
         self.m_logger.info('AVD %s, boot time is %s', avd, self.boot_time)
+        if 'apiP' in str(avd):
+            # Connectivity check for P
+            check_network_connectivity()
+            # Success if no exception, otherwise throw the exception all the way up
+            self.m_logger.info('ApiP network check succeeded.')
+
         launcher_emu.join(10)
         if not emu_argparser.emu_args.skip_adb_perf:
             self.run_adb_perf(avd)
