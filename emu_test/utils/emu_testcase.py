a157fd85a98a618ffa897843981a5c3de01f3239
bohu <bohu@google.com>
2018-01-24 07:14:07 -0800

    boot: add timeout value for run_with_timeout
    60 seconds should be enough
    
    Change-Id: I305b2e7475d8fbf7b2434b1f7dee40d38b3face0

--- emu_test/utils/emu_testcase.py
+++ emu_test/utils/emu_testcase.py
@@ -250,7 +250,7 @@ class EmuBaseTestCase(LoggedTestCase):
         """Launch given avd and wait for boot completion, return boot time"""
         """For ApiP, also check network connection"""
         def check_network_connectivity():
-            ret, output, _ = self.run_with_timeout(["adb", "shell", "dumpsys", "connectivity"])
+            ret, output, _ = self.run_with_timeout(["adb", "shell", "dumpsys", "connectivity"], 60)
             hasNumNetwork = False
             dnsSuccess = False
             networkCountKeyword = "Active default network: "
