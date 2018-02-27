b75785105e37999ab693115f406bd7a585efbcfb
David <dyu@google.com>
2018-02-27 11:24:32 -0800

    Add a camera test to verify photo capture.
    Test the photo capture functionality for APIs 24-27. Verifies the photo
    is saved on the device.
    
    TEST: local
    BUG: b/72697181
    Change-Id: Idb71aedcfa2bbf315ec52492d41937be5e00bd61

--- system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/common/Res.java
+++ system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/common/Res.java
@@ -64,6 +64,9 @@ public class Res {
     public static final String SETTINGS_RECYCLER_VIEW_RES =
             "com.android.settings:id/dashboard_container"; // Added in API 24
 
+    public static final String CAMERA_SHUTTER_BUTTON_RES = "com.android.camera2:id/shutter_button";
+    public static final String CAMERA_PHOTO_THUMBNAIL_RES = "com.android.camera2:id/rounded_thumbnail_view";
+    public static final String CAMERA_PHOTO_DELETE_RES = "com.android.camera2:id/filmstrip_bottom_control_delete";
     public static final String LOCK_SCREEN_ICON_RES = "com.android.systemui:id/lock_icon";
     public static final String MOBILE_TYPE_ICONS_RES = "com.android.systemui:id/mobile_type";
     public static final String VPN_LOCK_ICON_RES = "com.android.systemui:id/vpn";
---
+++ system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/watchers/CameraAccessPermissionsWatcher.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.devtools.systemimage.uitest.watchers;
+
+
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.UiWatcher;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Camera permissions popup watcher that monitors and dismisses settings confirmation popup dialogs.
+ */
+public class CameraAccessPermissionsWatcher implements UiWatcher {
+    private final UiDevice mDevice;
+
+    public CameraAccessPermissionsWatcher(UiDevice device) {
+        this.mDevice = device;
+    }
+
+    @Override
+    public boolean checkForCondition() {
+        boolean condition = false;
+        boolean hasPopup =
+                mDevice.findObject(new UiSelector().textMatches("(?i)allow(?-i)"))
+                        .waitForExists(TimeUnit.MILLISECONDS.convert(3L, TimeUnit.SECONDS));
+        try {
+            if (hasPopup) {
+                mDevice.findObject(new UiSelector().textMatches("(?i)allow(?-i)")).clickAndWaitForNewWindow();
+                condition = true;
+            }
+            hasPopup = mDevice.findObject(new UiSelector().textMatches("(?i)next(?-i)"))
+                    .waitForExists(TimeUnit.MILLISECONDS.convert(3L, TimeUnit.SECONDS));
+            if (hasPopup) {
+                mDevice.findObject(new UiSelector().textMatches("(?i)next(?-i)")).clickAndWaitForNewWindow();
+                condition = true;
+            }
+        }
+        catch (UiObjectNotFoundException e) {
+            throw new AssertionError("Unable to grant camera privilege");
+        }
+        return condition;
+    }
+}
