43f9dc1ffa31cb80ca98f42cc2e69e338ee33e27
David <dyu@google.com>
2018-04-02 14:15:47 -0700

    Change test result links to group by ClassName_AVD-type.
    Test: None
    BUG: b/77485310
    Change-Id: I522b6cbef443acf6748bc09434bc7ef50ae28332

--- emu_test/utils/emu_testcase.py
+++ emu_test/utils/emu_testcase.py
@@ -687,6 +687,9 @@ def create_test_case_from_file(desc, testcase_class, test_func, variants=None):
             func = func
         qemu_str = "_qemu2" if avd_config.classic == "no" else "_qemu1"
         variant_str = "_%s" % variant if variant is not None else ""
+        # Group test results by ClassName_AVD-type.
+        setattr(testcase_class, "%s%s_test_%s%s" % (variant_str, str(avd_config),
+                                                    desc, qemu_str), func)
         setattr(testcase_class, "test_%s_%s%s%s" % (desc, str(avd_config), qemu_str,
                                                     variant_str), func)
 
