c1b9401b7d6a2a5fe486197489dafa609043b8fc
Christopher Pilch <cpilch@google.com>
2018-04-20 12:05:46 -0700

    Document and modify homescreen and fingerprint tst
    Instead of relying on the local PATH to find adb, use the location
    defined from ANDROID_SDK_HOME.
    
    Additionally, use PEP8 standard comments for every function.
    
    A few functions were changed, however changes are noop.
    
    Test:  Ran locally.
    Change-Id: I222c45b3853d3b999d48988e033e444daced747d

--- emu_test/utils/test_fingerprint.py
+++ emu_test/utils/test_fingerprint.py
@@ -1,149 +1,247 @@
-#this is supposed to work on all screen size on pi
+"""
+Please note that the below test should work on all screen sizes for Pi images.
+Requires environment varaible 'ANDROID_SDK_HOMR' to be set due to path_utils.
+
+This file will attempt to create and then utilize a fingerprint based lock/unlock on the booted emulator image.
+"""
 
 import subprocess
 import sys
 import time
+import traceback
 import os
+import emu_test.utils.path_utils as path_utils
 import xml.etree.ElementTree as ET
 
+""" Serial for the running emulator process. """
 g_emulator_serial = 'emulator-5554'
-
 if len(sys.argv) == 2:
-    g_emulator_serial = sys.argv[1];
+    g_emulator_serial = sys.argv[1]
 
-def dump_screen_xml():
-    subprocess.call(['adb', '-s', g_emulator_serial, 'shell', 'uiautomator', 'dump'])
 
-def pull_xml(name):
-    subprocess.call(['adb','-s', g_emulator_serial,  'pull', '/sdcard/window_dump.xml', name])
+def dump_and_pull_screen_xml(filepath):
+    """
+    Call into ADB to dump out the current window_dump.xml file.  Then copy the dumped file to filepath.
+    :param filepath: Local filesystem path to copy the window_dump.xml file to.
+    :return: None.
+    """
+    adb_binary = path_utils.get_adb_binary()
+    subprocess.call([adb_binary, '-s', g_emulator_serial, 'shell', 'uiautomator', 'dump'])
+    subprocess.call([adb_binary, '-s', g_emulator_serial, 'pull', '/sdcard/window_dump.xml', filepath])
 
-def parse_xml(name):
-    tree = ET.parse(name)
+
+def parse_xml(filepath):
+    """
+    Parse the passed in XML file and return the root object of that file.
+    :param filepath: XML file we wish to parse.
+    :return: Root element of XML document.
+    """
+    tree = ET.parse(filepath)
     root = tree.getroot()
     return root
 
+
 def get_middle(bounds):
-    aa = bounds.replace('[', ' ');
-    bb = aa.replace(']', ' ');
-    cc = bb.replace(',', ' ');
-    dd = cc.split();
+    """
+    Retrieve the middle xy location of the passed in bounds tuple.
+    :param bounds: The Bounds of the region to calculate the middle of.  String of [x1,y1,x2,y2].
+    :return: [x,y] location representing the middle point of the region.
+    """
+    aa = bounds.replace('[', ' ')
+    bb = aa.replace(']', ' ')
+    cc = bb.replace(',', ' ')
+    dd = cc.split()
     [x1, y1, x2, y2] = dd
-    x = (int(x1) + int(x2)) / 2;
-    y = (int(y1) + int(y2)) / 2;
+    x = (int(x1) + int(x2)) / 2
+    y = (int(y1) + int(y2)) / 2
     return [str(x), str(y)]
 
 
 def find_location(root, name):
+    """
+    Given a XML tree and a target node name, find the target node and return its coordinate bounds.
+    :param root: XML tree object.
+    :param name: Name of the node.text element we wish to search for.
+    :return: List of string [x,y] coordinates of the middle of the element.  Or empty list if not found.
+    """
     for x in root.iter('node'):
         if name == x.get('text'):
-            return get_middle(x.get('bounds'));
-    print 'ERROR: cannot find ' + name +  ' button'
+            return get_middle(x.get('bounds'))
+    print 'ERROR: Failed to find ' + name + ' node element.'
     return []
 
-def get_button_location(name):
-    dump_screen_xml();
-    pull_xml(name + '.xml');
-    root = parse_xml(name + '.xml');
-    xy=find_location(root, name);
+
+def get_middle_location(name):
+    """
+    Get the location of the middle x,y coordinates of the given named element.
+    :param name: Name of the node element we wish to retrieve the
+    :return: Return of find_location() function.
+    """
+    dump_and_pull_screen_xml(name + '.xml')
+    root = parse_xml(name + '.xml')
+    xy = find_location(root, name)
     return xy
 
-def adb_touch(x,y):
+
+def adb_touch(x, y):
+    """
+    Perform a emulated touch action at the x,y coordinates passed.
+    :param x: X position where we wish to perform emulated touch.
+    :param y: Y position where we wish to perform emulated touch.
+    :return: None.
+    """
+    adb_binary = path_utils.get_adb_binary()
     time.sleep(2)
-    subprocess.call(['adb', 'shell', 'input', 'tap', x, y]);
+    subprocess.call([adb_binary, 'shell', 'input', 'tap', x, y])
     time.sleep(2)
 
+
 def adb_touch_button(name):
+    """
+    Attempts to simulate a button press on the passed in button name for the running emulator.
+    :param name: Name of the button element we wish to simulate a press on.
+    :return: None.
+    """
     tries = 0
     [x, y] = ['0', '0']
     status = False
     while tries < 10:
-        z = get_button_location(name)
-        if len(z) == 0:
-            print "cannot find the location, try again"
+        coordinates = get_middle_location(name)
+        if len(coordinates) == 0:
+            print "Failed to find the middle location of %s.  Will try again.", (name)
             time.sleep(2)
             tries = tries + 1
         else:
-            [x, y] = z
+            [x, y] = coordinates
             status = True
             break
-    os.remove(name + ".xml");
+    os.remove(name + ".xml")
     if not status:
-        raise Exception("Tried many times but still failed to find location of button " + name)
-    adb_touch(x,y)
+        raise Exception("Tried 10 times but still failed to find middle location of " + name)
+    adb_touch(x, y)
+
 
 def adb_input_text(textstr):
+    """
+    Perform a simulated text input on the running emulator, using the passed textstr as the simulated input.
+    :param textstr: String we wish to 'type' into the emulator.
+    :return: None.
+    """
+    adb_binary = path_utils.get_adb_binary()
     time.sleep(2)
-    subprocess.call(['adb', 'shell', 'input', 'text', textstr]);
+    subprocess.call([adb_binary, 'shell', 'input', 'text', textstr])
     time.sleep(2)
 
-def emu_console_finger_touch(x):
+
+def emu_console_finger_touch(finger):
+    """
+    Simulate a fingerprint touch on the running emulator.  'finger' is the registered fingerprint we are simulating.
+    :param finger: Int representing the finger we are simulating a touch for.  eg. '1' means simulate touch of first
+        registered finger.
+    :return: None.
+    """
+    adb_binary = path_utils.get_adb_binary()
     time.sleep(2)
-    subprocess.call(['adb', 'emu', 'finger', 'touch', x]);
+    subprocess.call([adb_binary, 'emu', 'finger', 'touch', finger])
     time.sleep(2)
+
 
 def adb_power_key():
+    """
+    Simulate touching the power button on the running emulator.  '26' maps to "KEYCODE_POWER" event code.
+    :return: None.
+    """
+    adb_binary = path_utils.get_adb_binary()
     time.sleep(2)
-    subprocess.call(['adb', 'shell', 'input', 'keyevent', '26']);
+    subprocess.call([adb_binary, 'shell', 'input', 'keyevent', '26'])
     time.sleep(2)
+
 
 def check_screen_is_unlocked():
-    ret1 = subprocess.call(['adb', 'shell', 'dumpsys', 'deviceidle', '|', 'grep',
-            'mScreenLocked=false']);
+    """
+    Check if the screen is currently unlocked on the running emulator.
+    :return: Boolean.  True if screen is unlocked.  False if screen is locked.
+    """
+    adb_binary = path_utils.get_adb_binary()
+    ret1 = subprocess.call([adb_binary, 'shell', 'dumpsys', 'deviceidle', '|', 'grep', 'mScreenLocked=false'])
     if ret1:
-        print 'fail to unlock screen'
+        print 'Emulated screen is not reporting as unlocked.'
         return False
     else:
-        print 'screen unlocked successfully'
+        print 'Emulated screen is reporting as unlocked.'
         return True
+
 
 def check_screen_is_locked():
-    ret1 = subprocess.call(['adb', 'shell', 'dumpsys', 'deviceidle', '|', 'grep',
-            'mScreenLocked=true']);
+    """
+    Check if the screen is currently locked on the running emulator.
+    :return: Boolean.  True if the screen is locked.  False if the screen is unlocked.
+    """
+    adb_binary = path_utils.get_adb_binary()
+    ret1 = subprocess.call([adb_binary, 'shell', 'dumpsys', 'deviceidle', '|', 'grep', 'mScreenLocked=true'])
     if ret1:
-        print 'fail to lock screen'
+        print 'Emulated screen is not reporting as locked.'
         return False
     else:
-        print 'screen locked successfully'
+        print 'Emulated screen is reporting as locked.'
         return True
 
-#this works on pi
+
+# The below function works on Pi.
 def do_fingerprint_test():
+    """
+    Perform a fingerprint test on the running emulator.  This is done in the following order:
+        1.  Enroll a fingerprint.
+        2.  Turn off the screen.
+        3.  Turn on the screen.
+        4.  Check that the screen reports as locked once powered on.
+        5.  Simulate a fingerprint touch.
+        6.  Check that the screen is reporting as unlocked.
+    :return:
+    """
     try:
-        subprocess.call(['adb', 'shell', 'am', 'start', '-n', \
-                'com.android.settings/com.android.settings.fingerprint.FingerprintEnrollIntroduction'])
-        adb_touch_button('NEXT');
-        adb_touch_button('Fingerprint + PIN');
-        adb_touch_button('NO');
-        adb_input_text('1111');
-        adb_touch_button('NEXT');
-        adb_input_text('1111');
-        adb_touch_button('CONFIRM');
-        adb_touch_button('DONE');
-        emu_console_finger_touch('1');
-        emu_console_finger_touch('1');
-        emu_console_finger_touch('1');
-        adb_touch_button('DONE');
-        #test a few times
-        for tries in range(0,3):
-            #turn off screen
-            adb_power_key();
-            #turn on screen
-            adb_power_key();
-            #check it is locked
-            check_screen_is_locked();
-            #touch fingerprint
-            emu_console_finger_touch('1');
-            #check it is unlocked
-            ret = check_screen_is_unlocked();
+        adb_binary = path_utils.get_adb_binary()
+        # Start the fingerprint enroll process.  Then simulate enrollment of finger '1'.
+        subprocess.call([adb_binary, 'shell', 'am', 'start', '-n',
+                         'com.android.settings/com.android.settings.fingerprint.FingerprintEnrollIntroduction'])
+        adb_touch_button('NEXT')
+        adb_touch_button('Fingerprint + PIN')
+        adb_touch_button('NO')
+        adb_input_text('1111')
+        adb_touch_button('NEXT')
+        adb_input_text('1111')
+        adb_touch_button('CONFIRM')
+        adb_touch_button('DONE')
+        emu_console_finger_touch('1')
+        emu_console_finger_touch('1')
+        emu_console_finger_touch('1')
+        adb_touch_button('DONE')
+        # Test that we successfully unlock the device.  We will try 3 times.
+        for tries in range(0, 3):
+            # Turn off the screen
+            adb_power_key()
+            # Turn on the screen
+            adb_power_key()
+            # Check if the device reports as locked
+            check_screen_is_locked()
+            # Simulate fingerprint touch.
+            emu_console_finger_touch('1')
+            # Check if the device reports as unlocked.
+            ret = check_screen_is_unlocked()
             if ret:
+                print 'Successfully tested fingerprint test.'
                 return True
         return False
     except Exception as e:
-        print 'exception happened in fingerprint test, error:', e
+        print 'Exception occurred while attempting fingerprint test'
+        print traceback.format_exc()
         return False
 
+
 if __name__ == '__main__':
-    ret = do_fingerprint_test()
-    if ret == False:
+    """
+    Note that this file requires environment variable 'ANDROID_SDK_HOME' to be set to properly function.
+    """
+    if not do_fingerprint_test():
         sys.exit(1)
 
--- emu_test/utils/test_homescreen.py
+++ emu_test/utils/test_homescreen.py
@@ -1,25 +1,38 @@
-#this is supposed to test homescreen for googleapis
-#they should have just the launcher at top activity
-#nothing else;
-#there are at least two types of issue
-#1. there is an activty on top of home activity
-#2. there is a pop up on top of home activity
-#we need to detect both
-#package="com.google.android.apps.nexuslauncher"
-#or package='com.android.launcher2"
-#or package='com.android.launcher3"
+"""
+The functions in this file are designed to test Homescreen interaction for googleapis AVD's.
+They should have just the launcher as the top activity nothing else.
+There are at least two types of potential issues:
+    1. There is an activity on top of home activity
+    2. There is a pop up on top of home activity
+We need to be able to detect and handle both situations.
 
+package="com.google.android.apps.nexuslauncher"
+or package='com.android.launcher2"
+or package='com.android.launcher3"
+"""
 
 import subprocess
 import sys
-import time
 import os
 import re
+import traceback
+import emu_test.utils.path_utils as path_utils
 import xml.etree.ElementTree as ET
 
-#there should be just one Stack when the guest is booted
-#and the stack topactivity should be the launcher
+"""
+When we boot the emulator, there should exist only one Stack.  On the top of that stack should be the launcher.
+"""
+
+""" EXPECTED_LAUNCHERS are the task names of the launchers we expect to find. """
+EXPECTED_LAUNCHERS = ['com.android.launcher', 'nexuslauncher']
+
+
 def find_num_stack(filename):
+    """
+    Determines the number of Stacks currently active by parsing the passed file and searching for 'Stack id'.
+    :param filename: File we will search for 'Stack id'
+    :return: Number of lines in file containing 'Stack id'.
+    """
     count = 0
     with open(filename) as f:
         for line in f:
@@ -27,86 +40,130 @@ def find_num_stack(filename):
                 count = count + 1
     return count
 
+
 def get_activity_name(line):
+    """
+    Return the top activity found from the line.  We find it by searching for regex.
+    :param line: Line we will apply regex search on.
+    :return: Empty string if no match found, regex match if found.
+    """
     p = re.compile(r"topActivity=ComponentInfo{.*\}")
     ms = p.findall(line)
     if ms:
         return ms[0]
+    print "Failed to find topActivity=ComponentInfo{.*\} regex."
     return ''
 
+
 def find_top_activity(filename):
+    """
+    For the passed filename, search through the file and look for a line with 'topActivity', and call
+    get_activity_name() on such a line to find the name of the top activity.
+    :param filename: File we will search for the topActivity string in.
+    :return: Empty string if filename does not contain matching top activity, name of top activity if found.
+    """
     with open(filename) as f:
         for line in f:
             if 'topActivity' in line:
-                return get_activity_name(line);
+                return get_activity_name(line)
+    print 'Failed to find topActivity in %s' % filename
     return ''
 
+
 def find_home_activity(filename):
+    """
+    Given the passed in filename, find the topActivity defined within and check if it matches either of the expected
+    launchers.
+    :param filename: File we will search for the topActivity in.
+    :return: Boolean.  True if expected launcher is found, False if expected launcher is not topActivity.
+    """
     num_stacks = find_num_stack(filename)
     if num_stacks > 1:
-        return False;
-    top_activity = find_top_activity(filename)
-    # aosp launcher
-    if 'com.android.launcher' in top_activity:
-        return True;
-    # nexus launcher
-    if 'nexus' in top_activity:
-        return True;
-    print "cannot fine launcher in ", top_activity
-    return False;
-
-def do_activity_test():
-    subprocess.call(['adb', 'shell', 'am', 'stack', 'list', '>', '/data/local/tmp/activity.txt'])
-    subprocess.call(['adb', 'pull', '/data/local/tmp/activity.txt'])
-    ret = find_home_activity('activity.txt')
-    if not ret:
+        print 'We have too many stacks.  Expected 1, got %s' % str(num_stacks)
         return False
-    return True
-
-def find_package(root, name):
-    for x in root.iter('node'):
-        if name in x.get('package'):
-            return True;
+    top_activity = find_top_activity(filename)
+    # If any of the EXPECTED_LAUNCHERS are the top activity, return true.
+    if any(launcher in top_activity for launcher in EXPECTED_LAUNCHERS):
+        return True
+    print "Did not detect expected launcher as top_activity.  Found %s, expected one of %s"\
+          % (top_activity, ",".join(EXPECTED_LAUNCHERS))
     return False
 
-def parse_xml(name):
-    tree = ET.parse(name)
+
+def do_activity_test():
+    """
+    Using adb, dump the current stack list and pull down the dump to the local filesystem.  Then check this dump file
+    to see if one of the EXPECTED_LAUNCHERS is labeled as the top_activity.
+    :return: Boolean.  Return of function find_home_activity.  True if EXPECTED_LAUNCHER is topActivity. False
+        otherwise.
+    """
+    adb_binary = path_utils.get_adb_binary()
+    subprocess.call([adb_binary, 'shell', 'am', 'stack', 'list', '>', '/data/local/tmp/activity.txt'])
+    subprocess.call([adb_binary, 'pull', '/data/local/tmp/activity.txt'])
+    return find_home_activity('activity.txt')
+
+
+def find_package(root, name):
+    """
+    Search all nodes in the passed in XML root for the passed name.
+    :param root: XML Root entry.
+    :param name: Name of the Node we are searching for.
+    :return:  Boolean. True if name is found in XML tree, False otherwise.
+    """
+    for x in root.iter('node'):
+        if name in x.get('package'):
+            return True
+    return False
+
+
+def parse_xml(filename):
+    """
+    Parse the passed in XML. Return the root of the parsed XML.
+    :param filename: The XML file we will parse.
+    :return: Root Element of the Parsed XML object.
+    """
+    tree = ET.parse(filename)
     root = tree.getroot()
     return root
 
+
 def do_popup_test():
-    subprocess.call(['adb', 'shell', 'uiautomator', 'dump'])
-    subprocess.call(['adb', 'pull', '/sdcard/window_dump.xml', 'topscreen.xml'])
-    root = parse_xml('topscreen.xml');
-    os.remove("topscreen.xml");
-    aosplauncher=find_package(root, 'com.android.launcher');
-    if aosplauncher:
-        print 'found aosp launcher'
-        return True;
-    nexuslauncher=find_package(root, 'nexuslauncher');
-    if nexuslauncher:
-        print 'found nexuslauncher'
-        return True;
+    """
+    Dump the current UI Window hierarchy and pull down this file. Search this file to see if we can find the target
+    EXPECTED_LAUNCHERS entry.
+    :return:  Boolean. True if we find one of the expected launchers. False otherwise.
+    """
+    adb_binary = path_utils.get_adb_binary()
+    subprocess.call([adb_binary, 'shell', 'uiautomator', 'dump'])
+    subprocess.call([adb_binary, 'pull', '/sdcard/window_dump.xml', 'topscreen.xml'])
+    root = parse_xml('topscreen.xml')
+    os.remove('topscreen.xml')
+    for launcher in EXPECTED_LAUNCHERS:
+        active_launcher = find_package(root, launcher)
+        if active_launcher:
+            return True
+    print 'Failed to find one of Launchers %s' % ",".join(EXPECTED_LAUNCHERS)
     return False
 
+
 def do_homescreen_test():
+    """
+    Perform a test to see if one of the EXPECTED_LAUNCHERS is active on the emulator task list.
+    :return: Boolean. True if we find a EXPECTED_LAUNCHER entry. False if we find no EXPECTED_LAUNCHER.
+    """
     try:
-        ret1 = do_activity_test();
-        if not ret1:
-            return False;
-        ret2 = do_popup_test();
-        if not ret2:
-            return False;
+        return do_activity_test() and do_popup_test()
     except Exception as e:
-        print 'exception happened in homescreen test, error:', e
+        print 'Exception happened during homescreen test. Exception:'
+        print traceback.format_exc()
         return False
-    return True;
 
 
 if __name__ == '__main__':
-    ret = do_homescreen_test()
-    if ret == False:
-        print 'fail'
+    """
+    Note that environment variable "ANDROID_SDK_HOME" must be set to resolve location of ADB binary.
+    """
+    if not do_homescreen_test():
+        print 'Homescreen Test suite Failed.'
         sys.exit(1)
-    print 'pass'
-
+    print 'Homescreen Test suite Passed.'
