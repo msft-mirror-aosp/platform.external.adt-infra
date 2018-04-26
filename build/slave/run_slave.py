e48ba05ba7e0f8ace434550e1f2fd10da3a60bd5
Christopher Pilch <cpilch@google.com>
2018-04-26 11:06:51 -0700

    Add ROOT_DIR to the run_slave.py PYTHONPATH
    Change-Id: I457e3ae3d8bc004d1eea4f2c2b00fc8668fa27e2

--- build/slave/run_slave.py
+++ build/slave/run_slave.py
@@ -283,7 +283,8 @@ def main():
     os.path.join(ROOT_DIR, 'build_internal', 'site_config'),
     os.path.join(ROOT_DIR, 'build_internal', 'symsrc'),
     SCRIPT_DIR,  # Include the current working directory by default.
-    BUILD_DIR, # Include the Build Dir for emu_test directory.
+    BUILD_DIR, # Include the Build Dir
+    ROOT_DIR, # This is for emu_test directory.
   ]
 
   # Need to update sys.path prior to the following imports.
