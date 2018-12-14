98652eea4d617864cfb3d7fa7a3bea020fa58020
Akash Agrawal <akagrawal@google.com>
2018-12-14 10:33:08 -0800

    Scripts to create Windows/Linux buildbots on GCE
    Scripts to create new buildbot instances on GCE for Linux and Windows.
    The new instance will be ready to use and can run emulator if needed.
    
    Test: Local
    Change-Id: I70171687f70a730dfad4e81b481de6dbdd103f20

---
+++ buildbot_script/create_linux_post_buildbot_gce.sh
@@ -0,0 +1,18 @@
+# Script used to create new instances of Emulator/System Image go/ab Linux Buildbots.
+# Script will create a machine named 'adt-emu-buildbot-X', where X is the first
+# argument to the script.
+if [ $# -ne 1 ]
+then echo "You must supply a positive integer to append to the adt-emu-buildbot- prefix.  Integer must not currently be in use"
+fi
+
+if [ $1 -lt 1 ]
+then echo "You must supply a positive integer"
+fi
+
+export TEMP_INSTANCE=adt-emu-buildbot-$1
+export PROJECT=android-studio-build
+export FULL_PROJECT=android-studio-build
+export ZONE=us-east1-b
+export MACHINE_TYPE=n1-highmem-32
+export API_SCOPES="https://www.googleapis.com/auth/logging.admin","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/userinfo.email","https://www.googleapis.com/auth/gerritcodereview"
+gcloud compute instances create $TEMP_INSTANCE --project $FULL_PROJECT --zone $ZONE --machine-type $MACHINE_TYPE --network default --scopes $API_SCOPES --image "adt-emu-buildbot" --boot-disk-size "1500GB" --boot-disk-type "pd-standard" --boot-disk-device-name "$TEMP_INSTANCE"
