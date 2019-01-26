440b1878fe776adca33b30eb7e04513d2b152375
David <dyu@google.com>
2019-01-25 16:17:07 -0800

    Update UI tests to support Q images.
    Test: local
    Change-Id: I981521350361a08ad8fe4919539888e8dc4c347b

--- system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/common/Res.java
+++ system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/common/Res.java
@@ -64,6 +64,8 @@ public class Res {
     public static final String SETTINGS_ADVANCED_OPTION_RES = "com.android.settings:id/advanced";
     public static final String SETTINGS_LIST_CONTAINER_RES =
             "(com.android.settings|android):id/(dashboard|list|dashboard_container|apps_list)";
+    public static final String Q_SETTINGS_LIST_CONTAINER_RES =
+            "(com.android.settings|android):id/(main_content_scrollable_container)";
 
     public static final String CAMERA_FRAME_RES = "com.android.camera2:id/camera_app_root";
     public static final String CAMERA_SHUTTER_BUTTON_RES = "com.android.camera2:id/shutter_button";
