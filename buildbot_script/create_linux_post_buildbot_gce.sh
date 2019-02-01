657355cb32cf07e1e57cec00d6e947cd9485af66
Akash Agrawal <akagrawal@google.com>
2019-02-01 10:16:55 -0800

    Buildbot image update
    1. Buildbot image has been updated to the latest version which includes
    a script to start buildbot.
    2. The setup script has been moved to buildbot image.
    
    Test: Local
    Change-Id: I62c9cf2a27e2e9d9081590fdd4a6ad6dd936fdfb

--- buildbot_script/create_linux_post_buildbot_gce.sh
+++ buildbot_script/create_linux_post_buildbot_gce.sh
@@ -7,6 +7,7 @@
 # adt-emu-buildbot-1    Includes changes for MSVC
 # adt-emu-buildbot-2    Includes set of permission fixes for MSVC
 # adt-emu-buildbot-3    Includes VNC server fix
+# adt-emu-buildbot-4    Includes script to start buildbot
 
 if [ $# -ne 1 ]
 then echo "You must supply a positive integer to append to the adt-emu-buildbot- prefix.  Integer must not currently be in use"
@@ -16,7 +17,7 @@ if [ $1 -lt 1 ]
 then echo "You must supply a positive integer"
 fi
 
-export IMAGE_NAME=adt-emu-buildbot-3
+export IMAGE_NAME=adt-emu-buildbot-4
 export TEMP_INSTANCE=adt-emu-buildbot-$1
 export PROJECT=android-studio-build
 export FULL_PROJECT=android-studio-build
@@ -25,8 +26,4 @@ export MACHINE_TYPE=n1-highmem-32
 export API_SCOPES="https://www.googleapis.com/auth/logging.admin","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/userinfo.email","https://www.googleapis.com/auth/gerritcodereview"
 gcloud compute instances create $TEMP_INSTANCE --project $FULL_PROJECT --zone $ZONE --machine-type $MACHINE_TYPE --network default --scopes $API_SCOPES --image $IMAGE_NAME --boot-disk-size "1500GB" --boot-disk-type "pd-standard" --boot-disk-device-name "$TEMP_INSTANCE"
 
-echo "wait for the instance to be up"
-sleep 15
-
-gcloud compute scp ./setup_bb.sh --project $FULL_PROJECT --zone $ZONE $TEMP_INSTANCE:
-gcloud compute ssh --project $FULL_PROJECT --zone $ZONE $TEMP_INSTANCE -- sudo su -c ./setup_bb.sh android-build
+echo "Login into $TEMP_INSTANCE and run start_bb.sh from home directory as user android-build"
