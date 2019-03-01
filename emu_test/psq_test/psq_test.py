6135d0589169d34b75c02bcf5b82a814b6b9a0d1
Akash Agrawal <akagrawal@google.com>
2019-02-28 16:08:12 -0800

    New scripts to move build/test to GCE
    Adding scripts to build and run emulator tests on gce.
    
    Test: Local
    Change-Id: I191ca28de065fd01666f1c41ee58d66471432b4a

---
+++ emu_test/psq_test/psq_test.py
@@ -0,0 +1,94 @@
+"""Class for PSQ Snapshot Test Runner Tests"""
+
+import os
+import time
+import psutil
+import shutil
+import subprocess
+
+import emu_test.utils.emu_testcase
+from emu_test.utils.emu_argparser import emu_args
+from emu_test.utils import path_utils
+from emu_test.utils import emu_argparser
+from emu_test.utils.emu_testcase import EmuBaseTestCase, AVDConfig
+from emu_test.utils.emu_error import *
+
+class PsqBootTestBase(EmuBaseTestCase):
+    def __init__(self, *args, **kwargs):
+        super(PsqBootTestBase, self).__init__(*args, **kwargs)
+        self.avd_config = None
+
+    @classmethod
+    def setUpClass(cls):
+        super(PsqBootTestBase, cls).setUpClass()
+
+    def kill_emulator(self):
+        self.m_logger.debug('First try - quit emulator by adb emu kill')
+        kill_proc = psutil.Popen(["adb", "emu", "kill"])
+        # check emulator process is terminated
+        result = self.term_check(timeout=5)
+        if not result:
+            self.m_logger.debug('Second try - quit emulator by psutil')
+            self.kill_proc_by_name(["emulator", "qemu-system"])
+            result = self.term_check(timeout=10)
+            self.m_logger.debug("term_check after psutil.kill - %s", result)
+        return result
+
+    def tearDown(self):
+        result = self.kill_emulator()
+        self.m_logger.info("Remove AVD inside of tear down")
+        # avd should be found $HOME/.android/avd/
+        avd_dir = os.path.join(os.path.expanduser('~'), '.android', 'avd')
+        try:
+            if result and self.start_proc:
+                self.start_proc.wait()
+            time.sleep(1)
+            self.kill_proc_by_name(["crash-service", "adb"])
+            os.remove(os.path.join(avd_dir, '%s.ini' % self.avd_config.name()))
+            shutil.rmtree(os.path.join(avd_dir, '%s.avd' % self.avd_config.name()), ignore_errors=True)
+        except Exception, e:
+            self.m_logger.error("Error in cleanup - %r", e)
+            pass
+
+class PsqSnapshotRunnerTestCase(PsqBootTestBase):
+    def __init__(self, *args, **kwargs):
+        super(PsqBootTestBase, self).__init__(*args, **kwargs)
+
+    @classmethod
+    def setUpClass(cls):
+        super(PsqBootTestBase, cls).setUpClass()
+
+    def run_and_log(self, cmd):
+        out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        self.m_logger.info(out)
+        return out
+
+    def run_snapshot_runner_test(self, avd_config):
+        avd = self.create_avd(avd_config)
+        launcher_emu, boot_time = self.launch_emu_no_kill(avd_config,
+                ["-feature", "SnapshotAdb,Offworld"])
+        prebuilts_path = os.path.join(path_utils.get_emu_test_path(), "prebuilts")
+        self.run_and_log(["adb", "install", os.path.join(prebuilts_path,
+                                                         "AndroidOffworld.apk")])
+        self.run_and_log(["adb", "install", os.path.join(prebuilts_path,
+                                                         "AndroidOffworld_unittests.apk")])
+        # We need to run SaveLoadTest and ForkTest. ForkTest is flaky so we disable it for now.
+        out = self.run_and_log(["adb", "shell", "am", "instrument", "-w", "-r", "-e", "debug",
+            "false", "-e", "class", "'com.google.android.offworld.examples.snapshot.SaveLoadTest'",
+            "com.google.android.offworld.examples.snapshot.test/"
+            + "androidx.test.runner.AndroidJUnitRunner"])
+
+        launcher_emu.join(10)
+        self.kill_emulator()
+
+        assert "OK (1 test)" in out
+
+if emu_args.config_file is not None:
+    emu_test.utils.emu_testcase.create_test_case_from_file("SnapshotRunner", PsqSnapshotRunnerTestCase, PsqSnapshotRunnerTestCase.run_snapshot_runner_test)
+
+if __name__ == '__main__':
+    os.environ["SHELL"] = "/bin/bash"
+    emu_argparser.emu_args = emu_argparser.get_parser().parse_args()
+    print emu_argparser.emu_args
+    sys.argv[1:] = emu_args.unittest_args
+    unittest.main()
---
+++ emu_test/utils/build_emu.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# This is used to build and test emulator binaries.
+# This will be invoked by aosp-emu-master-dev.
+
+OUT_DIR=$1
+DIST_DIR=$2
+BID=$3
+CPU=$4
+
+export ANDROID_HOME=$SDK_EMULATOR
+export ANDROID_SDK_ROOT=$SDK_EMULATOR
+
+BUILDERNAME="Linux_gce"
+OS="linux"
+if [[ $OSTYPE == *"darwin"* ]]
+then
+    BUILDERNAME="Mac"
+    OS="darwin"
+else
+    ps cax | grep vnc > /dev/null
+    if [ $? -eq 1 ]; then
+        echo "Start VNC server"
+        vncserver
+    fi
+fi
+
+SESSION_DIR=$DIST_DIR/testlogs
+mkdir -p $SESSION_DIR
+
+echo "Build Emulator"
+echo "tools/buildSrc/servers/build_tools.sh $OUT_DIR $DIST_DIR $BID $CPU"
+tools/buildSrc/servers/build_tools.sh $OUT_DIR $DIST_DIR $BID $CPU
+
+echo "Run unzip -o $DIST_DIR/sdk-repo-$OS-emulator-*.zip -d $SESSION_DIR/emu-master-dev"
+unzip -o $DIST_DIR/sdk-repo-$OS-emulator-*.zip -d $SESSION_DIR/emu-master-dev
+
+echo "Update SDK"
+echo "Run $ANDROID_HOME/tools/bin/sdkmanager --update"
+(yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses) & sleep 10 ; kill $!
+pkill yes
+$ANDROID_HOME/tools/bin/sdkmanager --update
+
+echo "Running Boot tests"
+echo "Remove any existing AVDs"
+echo "sudo rm -rf $ANDROID_AVD_HOME/*"
+sudo rm -rf $ANDROID_AVD_HOME/*
+
+echo "Run python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Boot_test --file_pattern 'test_boot.*' --config_file external/adt-infra/emu_test/config/boot_cfg_byob.csv --buildername $BUILDERNAME --filter '{"ori": "public"}' --generate_xml"
+python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Boot_test --file_pattern 'test_boot.*' --config_file external/adt-infra/emu_test/config/boot_cfg_byob.csv --buildername $BUILDERNAME --filter '{"ori": "public"}' --generate_xml
+
+echo "Zip perf data"
+zip -rm $SESSION_DIR/Boot_test/test.outputs/outputs.zip $SESSION_DIR/Boot_test/test.outputs/*.json
+sh -c "cd $SESSION_DIR && zip -rm perfdata.zip Boot_test/test.outputs/*"
+rm -rf $SESSION_DIR/Boot_test/test.outputs
+
+echo "Running Snapshot save/load tests"
+echo "Run python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Snapshot_test --file_pattern 'psq_test.*' --config_file external/adt-infra/emu_test/config/psq_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf"
+python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Snapshot_test --file_pattern 'psq_test.*' --config_file external/adt-infra/emu_test/config/psq_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf
+
+echo "Remove any empty file"
+find $SESSION_DIR -size  0 -print0 |xargs -0 rm --
+
+exit 0
