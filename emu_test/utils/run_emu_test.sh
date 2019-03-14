b27839033af072d8c4d3551fdba95716abe0951b
Akash Agrawal <akagrawal@google.com>
2019-03-14 12:28:02 -0700

    Make target fail if the test failed to run
    Adding extra checks to catch infra failures.
    
    Test: Local
    Change-Id: I2c59cc2aea0b64e29d0edaa5dbed920d33328cd0

--- emu_test/utils/run_emu_test.sh
+++ emu_test/utils/run_emu_test.sh
@@ -4,6 +4,7 @@
 # This will be invoked by aosp-emu-master-dev.
 
 DIST_DIR=$1
+STATUS=0
 
 export ANDROID_HOME=$SDK_EMULATOR
 export ANDROID_SDK_ROOT=$SDK_EMULATOR
@@ -35,8 +36,8 @@ echo "Run unzip -o $BUILD_DIR/sdk-repo-$OS-emulator-[0-9]*.zip -d $SESSION_DIR/e
 unzip -o $BUILD_DIR/sdk-repo-$OS-emulator-[0-9]*.zip -d $SESSION_DIR/emu-master-dev
 
 echo "Remove any existing AVDs"
-echo "sudo rm -rf $ANDROID_AVD_HOME/*"
-sudo rm -rf $ANDROID_AVD_HOME/*
+echo "rm -rf $ANDROID_AVD_HOME/*"
+rm -rf $ANDROID_AVD_HOME/*
 
 echo "Generate Perf Data"
 echo "Run python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Perf_test --file_pattern 'test_boot.*' --config_file external/adt-infra/emu_test/config/boot_cfg_byob.csv --buildername $BUILDERNAME --filter '{"ori": "public-perf"}' --generate_perf"
@@ -46,29 +47,58 @@ echo "Zip perf data"
 sh -c "cd $SESSION_DIR && zip -rm Perf_test/test.outputs/outputs.zip Perf_test/test.outputs/*.json"
 sh -c "cd $SESSION_DIR && zip -rm $DIST_DIR/perfgate_data.zip Perf_test/test.outputs/*"
 
+if [[ ! -f $DIST_DIR/perfgate_data.zip ]]
+then
+    STATUS=1
+fi
+
 echo "Running Boot tests"
 echo "Run python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Boot_test --file_pattern 'test_boot.*' --config_file external/adt-infra/emu_test/config/boot_cfg_byob.csv --buildername $BUILDERNAME --filter '{"ori": "public"}' --generate_xml"
 python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Boot_test --file_pattern 'test_boot.*' --config_file external/adt-infra/emu_test/config/boot_cfg_byob.csv --buildername $BUILDERNAME --filter '{"ori": "public"}' --generate_xml
 
+if [[ ! -f $SESSION_DIR/Boot_test/test_report.xml ]]
+then
+    STATUS=1
+fi
+
 echo "Running Console tests"
 echo "Remove any existing AVDs"
-echo "sudo rm -rf $ANDROID_AVD_HOME/*"
-sudo rm -rf $ANDROID_AVD_HOME/*
+echo "rm -rf $ANDROID_AVD_HOME/*"
+rm -rf $ANDROID_AVD_HOME/*
 
 echo "Run python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Console_test --file_pattern 'test_console.*' --config_file external/adt-infra/emu_test/config/console_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf"
 python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Console_test --file_pattern 'test_console.*' --config_file external/adt-infra/emu_test/config/console_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf
 
+if [[ ! -f $SESSION_DIR/Console_test/test_consoleTestResult.xml ]]
+then
+    STATUS=1
+fi
+
 echo "Running AVD tests"
 echo "Remove any existing AVDs"
-echo "sudo rm -rf $ANDROID_AVD_HOME/*"
-sudo rm -rf $ANDROID_AVD_HOME/*
+echo "rm -rf $ANDROID_AVD_HOME/*"
+rm -rf $ANDROID_AVD_HOME/*
 
 echo "Run python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir AVD_test --file_pattern '*launch_avd*.*' --config_file external/adt-infra/emu_test/config/avd_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf --generate_xml"
 python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir AVD_test --file_pattern '*launch_avd*.*' --config_file external/adt-infra/emu_test/config/avd_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf --generate_xml
 
+if [[ ! -f $SESSION_DIR/AVD_test/test_report.xml ]]
+then
+    STATUS=1
+fi
+
 echo "Running psq snapshot tests"
+echo "Remove any existing AVDs"
+echo "rm -rf $ANDROID_AVD_HOME/*"
+rm -rf $ANDROID_AVD_HOME/*
+
 echo "Run python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Snapshot_test --file_pattern 'psq_test.*' --config_file external/adt-infra/emu_test/config/psq_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf --generate_xml"
 python -u external/adt-infra/emu_test/dotest.py --loglevel DEBUG --session_dir $SESSION_DIR --emulator $SESSION_DIR/emu-master-dev/emulator/emulator --test_dir Snapshot_test --file_pattern 'psq_test.*' --config_file external/adt-infra/emu_test/config/psq_cfg_byob.csv --buildername $BUILDERNAME --skip-adb-perf --generate_xml
+
+if [[ ! -f $SESSION_DIR/Snapshot_test/test_report.xml ]]
+then
+    STATUS=1
+fi
 
 echo "Remove deployed emulator"
 echo "Run rm -rf $SESSION_DIR/emu-master-dev"
@@ -80,4 +110,4 @@ rm -rf /buildbot/prebuilt/*
 echo "Remove any empty file"
 find $SESSION_DIR -size  0 -print0 |xargs -0 rm --
 
-exit 0
+exit $STATUS
