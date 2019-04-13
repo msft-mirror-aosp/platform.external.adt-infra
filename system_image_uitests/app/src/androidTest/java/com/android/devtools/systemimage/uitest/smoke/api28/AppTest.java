e523297bbb4a8b9492838c7c8282f41ee06cf510
Akash Agrawal <akagrawal@google.com>
2019-04-13 10:48:54 -0700

    Fix for AppTesti for api 28
    Added a long click on the bookmarked Website name. Only on long clicking the
    Trash icon will appear which will then delete the bookmark successfully.
    
    Test: Local
    Change-Id: I887860715242e2eb45037f62b62cc61e1ec08dbd

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api28/AppTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api28/AppTest.java
@@ -198,17 +198,20 @@ public class AppTest {
             Log.d(TAG, "Searching for bookmark...");
             new AppWatcher(device).checkForCondition();
 
+            final UiObject bookmarkedSite = device.findObject(new UiSelector().textContains("ESPN"));
+
             assertTrue("Cannot find bookmark",
                     new Wait().until(new Wait.ExpectedCondition() {
                         @Override
                         public boolean isTrue() {
                             return device.findObject(
                                     new UiSelector().textContains(("kmarks"))).exists() &&
-                                    device.findObject(
-                                            new UiSelector().textContains("ESPN")).exists();
+                                    bookmarkedSite.exists();
                         }
                     })
             );
+
+            bookmarkedSite.dragTo(bookmarkedSite,20);
 
             final UiObject trashCan = device.findObject(new UiSelector().
                     description("Delete bokmarks"));
