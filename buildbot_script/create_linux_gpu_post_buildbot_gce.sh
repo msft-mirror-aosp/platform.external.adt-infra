fd8d0e1864dea0f93cb996d44ef7c76c6dfd3f18
Akash Agrawal <akagrawal@google.com>
2019-04-22 06:47:41 -0700

    Update gpu type in buildbot script
    As our use case is for graphics, we have to use cws type of gpu.
    
    Test: Local
    Change-Id: Ib8949f71039436d636ef59d9cee324235de05c82

--- buildbot_script/create_linux_gpu_post_buildbot_gce.sh
+++ buildbot_script/create_linux_gpu_post_buildbot_gce.sh
@@ -20,7 +20,7 @@ export FULL_PROJECT=android-studio-build
 export ZONE=us-east1-b
 export MACHINE_TYPE=n1-highmem-16
 export ACCELERATOR_COUNT=1
-export ACCELERATOR_TYPE=nvidia-tesla-p100
+export ACCELERATOR_TYPE=nvidia-tesla-p100-vws
 export API_SCOPES="https://www.googleapis.com/auth/logging.admin","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/pubsub","https://www.googleapis.com/auth/devstorage.read_write","https://www.googleapis.com/auth/userinfo.email","https://www.googleapis.com/auth/gerritcodereview"
 gcloud compute instances create $TEMP_INSTANCE --project $FULL_PROJECT --zone $ZONE --machine-type $MACHINE_TYPE --accelerator type=$ACCELERATOR_TYPE,count=$ACCELERATOR_COUNT --network default --scopes $API_SCOPES --image $IMAGE_NAME --boot-disk-size "1500GB" --boot-disk-type "pd-standard" --maintenance-policy TERMINATE --boot-disk-device-name "$TEMP_INSTANCE"
 
