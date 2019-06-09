954a7c2540539fb27daf49d607058ecab7707a63
Lingfeng Yang <lfy@google.com>
2019-06-09 07:52:38 +0000

    Revert "Revert "Update buildbot create script""
    This reverts commit 3f70492459a2368697b5e49d8c777f74364d58c9.
    
    Reason for revert: just trying out stuff
    
    Change-Id: Ifff8037fd3345ecb775c9b903546994331002ad1

--- buildbot_script/create_linux_post_buildbot_gce.sh
+++ buildbot_script/create_linux_post_buildbot_gce.sh
@@ -8,6 +8,7 @@
 # adt-emu-buildbot-2    Includes set of permission fixes for MSVC
 # adt-emu-buildbot-3    Includes VNC server fix
 # adt-emu-buildbot-4    Includes script to start buildbot
+# adt-emu-buildbot-5    mount src to tmpfs
 
 if [ $# -ne 1 ]
 then echo "You must supply a positive integer to append to the adt-emu-buildbot- prefix.  Integer must not currently be in use"
@@ -17,7 +18,7 @@ if [ $1 -lt 1 ]
 then echo "You must supply a positive integer"
 fi
 
-export IMAGE_NAME=adt-emu-buildbot-4
+export IMAGE_NAME=adt-emu-buildbot-5
 export TEMP_INSTANCE=adt-emu-buildbot-$1
 export PROJECT=android-studio-build
 export FULL_PROJECT=android-studio-build
