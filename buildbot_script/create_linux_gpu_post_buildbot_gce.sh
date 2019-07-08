8baa1978ca63f2c456b84865004c682780751f02
Akash Agrawal <akagrawal@google.com>
2019-07-08 11:11:44 -0700

    Changes to buildbot scripts
    We have buildbot as startup service for Linux images. Therefore, we
    will be using 2 versions now
    
    prod - image with stratup service set
    test - image without the startup service so that this can be used to
    create test gce machines without starting buildbot on it.
    
    Test: Local
    Change-Id: I5f881553c2dfe73077a3b1a18107b6bb878296f5

--- buildbot_script/create_linux_gpu_post_buildbot_gce.sh
+++ buildbot_script/create_linux_gpu_post_buildbot_gce.sh
@@ -3,17 +3,33 @@
 # argument to the script and perform a basic setup.
 #
 # IMAGE VERSION HISTORY
-# adt-emu-gpu-buildbot-1      Base Image to run emulator and system image tests
+# adt-emu-gpu-buildbot-1        Base Image to run emulator and system image tests
+# adt-emu-gpu-buildbot-2        Fixes nvidia module issue after reboot
+# adt-emu-gpu-buildbot-2-test   Image to be used for test machines
+#                                (does not start buildbot)
+# adt-emu-gpu-buildbot-2-prod   Image to be used for buildbots
+#                                (starts buildbot on startup)
 
-if [ $# -ne 1 ]
-then echo "You must supply a positive integer to append to the adt-emu-gpu-buildbot- prefix.  Integer must not currently be in use"
+if [ $# -ne 2 ]
+then
+    echo "You must supply a positive integer to append to the adt-emu-buildbot- prefix.  Integer must not currently be in use"
+    echo "You must supply an image type, either prod or test."
+    exit -1
 fi
 
 if [ $1 -lt 1 ]
-then echo "You must supply a positive integer"
+then
+    echo "You must supply a positive integer"
+    exit -1
 fi
 
-export IMAGE_NAME=adt-emu-gpu-buildbot-1
+export IMAGE_TYPE="test"
+if [ $2 == "prod" ]
+then
+   export IMAGE_TYPE="prod"
+fi
+
+export IMAGE_NAME=adt-emu-gpu-buildbot-2-$IMAGE_TYPE
 export TEMP_INSTANCE=adt-emu-gpu-buildbot-$1
 export PROJECT=android-studio-build
 export FULL_PROJECT=android-studio-build
--- buildbot_script/create_linux_post_buildbot_gce.sh
+++ buildbot_script/create_linux_post_buildbot_gce.sh
@@ -3,22 +3,37 @@
 # argument to the script and perform a basic setup.
 #
 # IMAGE VERSION HISTORY
-# adt-emu-buildbot      Base Image to run emulator and system image tests
-# adt-emu-buildbot-1    Includes changes for MSVC
-# adt-emu-buildbot-2    Includes set of permission fixes for MSVC
-# adt-emu-buildbot-3    Includes VNC server fix
-# adt-emu-buildbot-4    Includes script to start buildbot
-# adt-emu-buildbot-5    mount src to tmpfs
+# adt-emu-buildbot          Base Image to run emulator and system image tests
+# adt-emu-buildbot-1        Includes changes for MSVC
+# adt-emu-buildbot-2        Includes set of permission fixes for MSVC
+# adt-emu-buildbot-3        Includes VNC server fix
+# adt-emu-buildbot-4        Includes script to start buildbot
+# adt-emu-buildbot-5        mount src to tmpfs
+# adt-emu-buildbot-5-test   Image to be used for test machines
+#                            (does not start buildbot)
+# adt-emu-buildbot-5-prod   Image to be used for buildbots
+#                            (starts buildbot on startup)
 
-if [ $# -ne 1 ]
-then echo "You must supply a positive integer to append to the adt-emu-buildbot- prefix.  Integer must not currently be in use"
+if [ $# -ne 2 ]
+then
+    echo "You must supply a positive integer to append to the adt-emu-buildbot- prefix.  Integer must not currently be in use"
+    echo "You must supply an image type, either prod or test."
+    exit -1
 fi
 
 if [ $1 -lt 1 ]
-then echo "You must supply a positive integer"
+then
+    echo "You must supply a positive integer"
+    exit -1
 fi
 
-export IMAGE_NAME=adt-emu-buildbot-5
+export IMAGE_TYPE="test"
+if [ $2 == "prod" ]
+then
+   export IMAGE_TYPE="prod"
+fi
+
+export IMAGE_NAME=adt-emu-buildbot-5-$IMAGE_TYPE
 export TEMP_INSTANCE=adt-emu-buildbot-$1
 export PROJECT=android-studio-build
 export FULL_PROJECT=android-studio-build
