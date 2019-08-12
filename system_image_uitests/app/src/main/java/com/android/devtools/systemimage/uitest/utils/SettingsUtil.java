08162b8f76d3f9b33885555d308dc8d5d697347a
David <dyu@google.com>
2019-08-12 13:22:06 -0700

    Extend wait time for app permission list to render.
    Recent test failure shows test runner is waiting for list of application
    permissions to appear on the UI. Increase maximum wait time to avoid
    test from timing out. Spinner was still up and app list didn't load
    after 20 secs. May need to account for lag.
    
    TEST: local.
    BUG: b/138401038
    Change-Id: If23409f1dfbf48f4f5b157854cd996a6a68de41f

--- system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/utils/SettingsUtil.java
+++ system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/utils/SettingsUtil.java
@@ -228,7 +228,7 @@ public class SettingsUtil {
             appPermissionsLabel.clickAndWaitForNewWindow();
         }
         UiScrollable appPermissionsList = new UiScrollable(new UiSelector().resourceId(Res.ANDROID_CONTENT_RES));
-        if (appPermissionsList.waitForExists(TimeUnit.SECONDS.toMillis(20))) {
+        if (appPermissionsList.waitForExists(TimeUnit.SECONDS.toMillis(30L))) {
             return appPermissionsList.getChildByText(new UiSelector().className("android.widget.TextView"), appType);
         } else {
             throw new UiObjectNotFoundException("Failed to find the item in App permissions.");
