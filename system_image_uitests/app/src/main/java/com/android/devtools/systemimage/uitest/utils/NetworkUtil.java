fd6af35be723fb0ecf7a1f8607003f1fd0dc0866
Akash Agrawal <akagrawal@google.com>
2019-08-16 10:10:20 -0700

    Fix for NetworkIOTest
    Refactor openExtendedNotificationsPanel to a simplified process. Also
    update isAirplaneModeEnabled to press home at the end.
    
    Bug: 139533117
    Test: Local
    Change-Id: I23629d7fa8d940fd977c7c91c0830d9c7c236853

--- system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/utils/NetworkUtil.java
+++ system_image_uitests/app/src/main/java/com/android/devtools/systemimage/uitest/utils/NetworkUtil.java
@@ -22,6 +22,7 @@ import com.android.devtools.systemimage.uitest.watchers.watcher;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
@@ -105,36 +106,18 @@ public class NetworkUtil {
             status = airplaneModeView.waitForExists(5L) && airplaneModeView.getContentDescription().toLowerCase().contains("on");
         }
 
-        UiObject expandIndicator = device.findObject(
-                new UiSelector().resourceId("com.android.systemui:id/qs_navbar_scrim"));
-        expandIndicator.click();
+        device.pressHome();
+
         return status;
     }
 
-    public static void openExtendedNotificationsPanel(UiDevice device)
-            throws UiObjectNotFoundException {
+    public static void openExtendedNotificationsPanel(UiDevice device) throws UiObjectNotFoundException {
+        device.pressHome();
+
+        UiObject pan = device.findObject(new UiSelector().resourceId("com.google.android.apps.nexuslauncher:id/scrim_view"));
+        Rect panRect = pan.getBounds();
+
         device.openNotification();
-        new watcher(device, Res.NETWORK_UTIL_WATCHER_PATTERN).checkForCondition();
-
-        UiObject expandIndicator = device.findObject(
-                new UiSelector().resourceId(Res.NOTIFICATION_BAR_EXPAND_RES));
-        if (expandIndicator.waitForExists(5L)) {
-            expandIndicator.clickAndWaitForNewWindow();
-        }
-
-        UiObject quickPanel = device.findObject(new UiSelector().
-                resourceId(Res.NOTIFICATION_QUICK_PANEL_RES));
-        if (quickPanel.waitForExists(3L)) {
-            Context context = testFramework.getInstrumentation().getContext();
-            DisplayMetrics metrics = new DisplayMetrics();
-            WindowManager windowManager = (WindowManager) context.
-                    getSystemService(Context.WINDOW_SERVICE);
-            windowManager.getDefaultDisplay().getMetrics(metrics);
-            Display display = windowManager.getDefaultDisplay();
-            Point point = new Point();
-            display.getSize(point);
-            int maxY = point.y;
-            quickPanel.dragTo(0, maxY/2, 5);
-        }
+        device.drag(panRect.left, panRect.top, panRect.left, panRect.centerY(), 10);
     }
 }
