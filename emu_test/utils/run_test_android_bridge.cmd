532325dc8f19449f461f04af5f598062db125069
Akash Agrawal <akagrawal@google.com>
2019-08-20 16:33:39 -0700

    Script to run ADB test on windows
    Adding script to run ADB test on windows
    
    Test: Local
    Change-Id: I98b2b33e51caeddb82711c9348802727062fb5ef

---
+++ emu_test/utils/run_test_android_bridge.cmd
@@ -0,0 +1,29 @@
+@setlocal enabledelayedexpansion
+@echo off
+
+REM This is used to run ADB tests.
+REM This will be invoked by system image source.
+REM {src}/platform_testing/emu_test/run_test.cmd
+
+set DISTRIB_DIR=%1
+
+setx ANDROID_HOME %SDK_SYS_IMAGE% /M
+setx ANDROID_SDK_ROOT %SDK_SYS_IMAGE% /M
+setx ANDROID_EMU_ENABLE_CRASH_REPORTING "NO" /M
+
+call refreshenv
+
+set SESSION_DIR=%DISTRIB_DIR%\testlogs
+mkdir %SESSION_DIR%
+
+echo "Run python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900"
+python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900
+
+echo "UI test completed, kill adb server"
+cmd.exe /c %ANDROID_HOME%\platform-tools\adb.exe kill-server
+
+echo "Cleanup empty files"
+for /f %%d in ('dir /s /b /A:-D %SESSION_DIR%') do (if %%~zd==0 del %%d)
+
+echo "ADB test completed"
+exit 0
