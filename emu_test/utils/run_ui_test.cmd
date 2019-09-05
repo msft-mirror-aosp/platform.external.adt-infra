9f9929021b2fec123ce30fe0964ff912f0029924
Akash Agrawal <akagrawal@google.com>
2019-09-05 11:55:23 -0700

    Fix killing hanged adb
    On windows, adb hangs a lot of times at the end of the test suite. This
    prevents buildbot from moving on to the next build. I have fixed adb
    kill script to keep trying untill it finds a running instance of adb.
    
    Test: Local
    Change-Id: Iff7eb14b90d22acd1aebec1dc6f4627cb48caa76

--- emu_test/utils/run_ui_test.cmd
+++ emu_test/utils/run_ui_test.cmd
@@ -50,7 +50,7 @@ rmdir /s /q %SNAPSHOT_DIR%
 
 echo "UI test completed, kill adb server"
 cmd.exe /c %ANDROID_HOME%\platform-tools\adb.exe kill-server
-python -u %ADT_INFRA%\emu_test\utils\kill_adb.py
+python -u %ADT_INFRA%\emu_test\utils\kill_android_bridge_server.py
 
 echo "Cleanup empty files"
 for /f %%d in ('dir /s /b /A:-D %SESSION_DIR%') do (if %%~zd==0 del %%d)
