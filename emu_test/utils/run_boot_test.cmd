85ea29e9b461cce1824c29be9c5629fa9d74ec80
Akash Agrawal <akagrawal@google.com>
2019-09-16 12:18:54 -0700

    Add logging for test scripts on windows
    As test scripts are invoked in a command prompt of their own, we were
    loosing on logs. With this change, we will start collecting logs in a
    file under testlogs.
    
    Test: Local
    Change-Id: Icc4787460ce8db25c6128ff82cf41d10ba610236

--- emu_test/utils/run_boot_test.cmd
+++ emu_test/utils/run_boot_test.cmd
@@ -7,27 +7,36 @@ REM {src}/platform_testing/ui_test/run_ui_test.cmd
 
 title run_boot_test
 
-setx ANDROID_EMU_ENABLE_CRASH_REPORTING "NO" /M
-call refreshenv
-
 set DISTRIB_DIR=%1
 set ORI=%2
 set API=%3
 
-echo "Runnig BOOT test for %API%"
+setx ANDROID_EMU_ENABLE_CRASH_REPORTING "NO" /M
+call refreshenv
 
 set SESSION_DIR=%DISTRIB_DIR%\testlogs
 mkdir %SESSION_DIR%
+
+set LOGFILE=%SESSION_DIR%\run_boot_test.log
+call :LOG > %LOGFILE% 2>&1
+exit /B 0
+
+:LOG
+echo "Runnig BOOT test for %API%"
 
 set FILTER={\"ori\":\"%ORI%\"}
 
 echo "Run python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator-headless --test_dir BOOT_test --file_pattern test_boot.* --config_file %ADT_INFRA%\emu_test\config\boot_cfg_byob.csv --buildername 'Windows_gce' --filter %FILTER% --generate_xml --timeout 900"
 python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator-headless --test_dir BOOT_test --file_pattern test_boot.* --config_file %ADT_INFRA%\emu_test\config\boot_cfg_byob.csv --buildername 'Windows_gce' --filter %FILTER% --generate_xml --timeout 900
 
+echo "List running processes before killing ADB"
+C:\PSTools\tlist.exe /c
+
 echo "Boot test completed, kill adb server"
 cmd.exe /c %ANDROID_HOME%\platform-tools\adb.exe kill-server
 
+echo "List running processes after killing ADB"
+C:\PSTools\tlist.exe /c
+
 echo "Cleanup empty files"
 for /f %%d in ('dir /s /b /A:-D %SESSION_DIR%') do (if %%~zd==0 del %%d)
-
-exit 0
--- emu_test/utils/run_test_android_bridge.cmd
+++ emu_test/utils/run_test_android_bridge.cmd
@@ -16,15 +16,25 @@ call refreshenv
 set SESSION_DIR=%DISTRIB_DIR%\testlogs
 mkdir %SESSION_DIR%
 
+set LOGFILE=%SESSION_DIR%\run_test_android_bridge.log
+call :LOG > %LOGFILE% 2>&1
+exit /B 0
+
+:LOG
 echo "Run python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900"
 python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900
 
-echo "UI test completed, kill adb server"
+echo "List running processes before killing ADB"
+C:\PSTools\tlist.exe /c
+
+echo "ADB test completed, kill adb server"
 cmd.exe /c %ANDROID_HOME%\platform-tools\adb.exe kill-server
 python -u %ADT_INFRA%\emu_test\utils\kill_android_bridge_server.py
+
+echo "List running processes after killing ADB"
+C:\PSTools\tlist.exe /c
 
 echo "Cleanup empty files"
 for /f %%d in ('dir /s /b /A:-D %SESSION_DIR%') do (if %%~zd==0 del %%d)
 
 echo "ADB test completed"
-exit 0
