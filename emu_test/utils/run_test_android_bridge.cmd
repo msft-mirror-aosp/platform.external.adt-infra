b83abfc4424efbc3863be72b470a44a9a5fabd4b
Akash Agrawal <akagrawal@google.com>
2019-09-23 15:33:02 -0700

    Windows script fixes
    Add title for windows script for command prompt. This title is being
    used by other script to detect timeout situtions.
    Use emulator-headless
    
    Test: Local
    Change-Id: I8b8e6e9d8f3bf42b0bc3893f0a539c64fb82b0a8

--- emu_test/utils/run_test_android_bridge.cmd
+++ emu_test/utils/run_test_android_bridge.cmd
@@ -5,6 +5,8 @@ REM This is used to run ADB tests.
 REM This will be invoked by system image source.
 REM {src}/platform_testing/emu_test/run_test.cmd
 
+title run_test_android_bridge
+
 set DISTRIB_DIR=%1
 
 setx ANDROID_HOME %SDK_SYS_IMAGE% /M
@@ -23,8 +25,8 @@ call :LOG > %LOGFILE% 2>&1
 exit /B 0
 
 :LOG
-echo "Run python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900"
-python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900
+echo "Run python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator-headless --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900"
+python -u %ADT_INFRA%\emu_test\dotest.py --loglevel DEBUG --session_dir %SESSION_DIR% --emulator %ANDROID_SDK_ROOT%\emulator\emulator-headless --test_dir ADB_test --file_pattern test_adb.* --config_file %ADT_INFRA%\emu_test\config\adb_cfg_byob.csv --buildername Windows_gce --filter {\"ori\":\"public\"} --timeout 900
 
 echo "List running processes before killing ADB"
 C:\PSTools\tlist.exe /c
