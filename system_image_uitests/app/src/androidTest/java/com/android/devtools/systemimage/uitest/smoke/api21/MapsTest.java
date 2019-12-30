c95cff5a4505c2d7d107df3d29ae3e43653015d6
David <dyu@google.com>
2019-12-30 10:06:57 -0800

    Fix UI test for MapsTests in API 21.
    Test: local
    Change-Id: I4a403980b401a310eb09ea7fae8eb1b8685be12d

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api21/MapsTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api21/MapsTest.java
@@ -31,6 +31,7 @@ import com.android.devtools.systemimage.uitest.framework.SystemImageTestFramewor
 import com.android.devtools.systemimage.uitest.utils.AppLauncher;
 import com.android.devtools.systemimage.uitest.utils.Wait;
 import com.android.devtools.systemimage.uitest.watchers.MapsWatcher;
+import com.android.devtools.systemimage.uitest.watchers.watcher;
 
 import org.junit.Assert;
 import org.junit.Rule;
@@ -68,7 +69,7 @@ public class MapsTest {
      *   5. Enter search query, "San Francisco", select it from the auto fill results.
      *   6. "San Francisco" location card opens.
      *   7. Select "San Francisco".
-     *   8. Tap on the Drive icon.
+     *   8. Tap on the Route icon.
      *   Verify:
      *   1. Map points to San Francisco location.
      *   2. Navigation overview is displayed.
@@ -80,80 +81,42 @@ public class MapsTest {
         Instrumentation instrumentation = testFramework.getInstrumentation();
         UiDevice mDevice = testFramework.getDevice();
 
-        if (testFramework.isGoogleApiImage() || testFramework.isGoogleApiAndPlayImage()) {
+        if (testFramework.isGoogleApiImage()) {
             AppLauncher.launch(instrumentation, "Maps");
 
-            new MapsWatcher(mDevice).checkForCondition();
+            new watcher(mDevice, Res.MAPS_WATCHER_PATTERN).checkForCondition();
 
-            final UiObject searchUiObject = mDevice.findObject(new UiSelector().
-                    resourceIdMatches(Res.SEARCH_TEXT_BOX));
-            assertTrue("Failed to find search text box", new Wait(5L).until(new Wait.ExpectedCondition() {
-                @Override
-                public boolean isTrue() throws Exception {
-                    return searchUiObject.exists();
-                }
-            }));
-
-            searchUiObject.clickAndWaitForNewWindow();
-
-            UiObject searchEditText;
-            UiObject selectedLocation;
-            searchEditText = searchUiObject.getChild(new UiSelector().className(EditText.class.getName()));
-            searchEditText.setText(QUERY_STRING);
-            UiScrollable scrollView = new UiScrollable(new UiSelector().className(ScrollView.class.getName()));
-
-            final UiObject locationString = mDevice.findObject(new UiSelector().text(QUERY_STRING));
-            boolean hasLocationString = new Wait().until(new Wait.ExpectedCondition() {
-                @Override
-                public boolean isTrue() {
-                    return locationString.exists();
-                }
-            });
-
-            if (hasLocationString) {
-                scrollView.scrollIntoView(locationString);
+            final UiObject searchEditText = mDevice.findObject(new UiSelector()
+                .resourceIdMatches(Res.SEARCH_TEXT_BOX)
+            );
+            if (searchEditText.waitForExists(3000)) {
+                searchEditText.click();
+                searchEditText.setText(QUERY_STRING);
             }
 
-            selectedLocation = scrollView.getChildByText(new UiSelector()
-                    .className(TextView.class.getName()), QUERY_STRING);
+            final UiObject locationString = mDevice.findObject(new UiSelector().text(QUERY_STRING));
             Assert.assertTrue("Selected location " + QUERY_STRING + " not found.",
-                    selectedLocation.exists());
-            selectedLocation.clickAndWaitForNewWindow();
+                new Wait().until(locationString::exists));
+            locationString.clickAndWaitForNewWindow();
 
-            // Verify the Query String is present after completing search.
-            final UiObject searchTextView =
-                    searchUiObject.getChild(new UiSelector().className(TextView.class.getName()));
-
-            boolean hasSearchText = new Wait().until(new Wait.ExpectedCondition() {
-                @Override
-                public boolean isTrue() {
-                    return searchTextView.exists();
-                }
-            });
+            boolean hasSearchText = new Wait().until(searchEditText::exists);
 
             if (hasSearchText) {
                 Assert.assertTrue("Search string " + QUERY_STRING + " not found.",
-                        searchTextView.getText().contains(QUERY_STRING));
+                        searchEditText.getText().contains(QUERY_STRING));
             }
 
-            // Verify the directions/route link exists and clicking on it opens the directions page
-            // verify query string is pre filled in the destination("to") field.
-            UiObject directions;
-            boolean isSuccess = mDevice.findObject(new UiSelector().descriptionMatches(".*Directions.*|.*Route.*"))
-                    .waitForExists(TimeUnit.MILLISECONDS.convert(3L, TimeUnit.SECONDS));
+            // Verify that the route icon exists and clicking on it opens the directions page.
+            UiObject routeIcon = mDevice.findObject(
+                new UiSelector().descriptionMatches(".*Route.*"));
+            Assert.assertTrue("Could not find route icon",
+                routeIcon.waitForExists(3000));
+            routeIcon.clickAndWaitForNewWindow();
 
-            if (isSuccess) {
-                directions = mDevice.findObject(new UiSelector().descriptionMatches(".*Directions.*|.*Route.*"));
-            } else {
-                directions = mDevice.findObject(new UiSelector().text("DIRECTIONS"));
-            }
-            Assert.assertTrue(directions.exists());
-            directions.clickAndWaitForNewWindow();
-
+            // Verify that the query string is pre filled in the destination field.
             UiObject destination = mDevice.findObject(new UiSelector().textContains(QUERY_STRING));
-            new MapsWatcher(mDevice).checkForCondition();
-
-            Assert.assertTrue(destination.exists());
+            Assert.assertTrue("Could not find destination icon",
+                destination.waitForExists(3000));
 
             for (int i = 0; i < 5; i++) {
                 mDevice.pressBack();
