fd491156282aac67e83a4e6efa09d7d88d144285
Akash Agrawal <akagrawal@google.com>
2020-05-14 16:21:43 -0700

    UI utility for adb wireless
    To adb wireless, we need to interact with the phone ui. This utility has
    functions to do that.
    
    Bug: 155195230
    Change-Id: I0a557450f9ef35df74db4c6b753b54f685509df9

---
+++ adb_wireless_test/app/src/androidTest/java/com/android/devtools/adbtestutils/Framework.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.devtools.adbtestutils;
+
+import org.junit.Assert;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import android.app.Instrumentation;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.os.RemoteException;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.util.Log;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+/**
+ * Framework to execute ADB wireless test utility tasks.
+ **/
+public class Framework implements TestRule {
+
+    private final Instrumentation mInstrumentation = InstrumentationRegistry.getInstrumentation();
+    private final UiDevice mDevice = UiDevice.getInstance(mInstrumentation);
+    private static final int RETRY_COUNT = 2;
+
+    public Instrumentation getInstrumentation() {
+        return mInstrumentation;
+    }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state) && !Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
+    }
+
+    private boolean checkWriteExternalPermission()
+    {
+        String permission = "android.permission.WRITE_EXTERNAL_STORAGE";
+        int res = mInstrumentation.getContext().checkCallingOrSelfPermission(permission);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private File getLoggingDir(String testClassName, String testMethodName) {
+        Assert.assertTrue("Failed to write to external storage.", isExternalStorageWritable());
+        Assert.assertTrue("Failed to acquire permission.", checkWriteExternalPermission());
+        File externalStorageLogDir =
+                new File(Environment.getExternalStorageDirectory().getPath(), "Logs");
+        if (!externalStorageLogDir.exists())
+            externalStorageLogDir.mkdir();
+        File loggingDir = new File(new File(externalStorageLogDir.getPath(), testClassName),
+                testMethodName);
+        loggingDir.mkdirs();
+        return loggingDir;
+    }
+
+    private void resetDeviceState() throws RemoteException, UiObjectNotFoundException {
+        // Close all recently opened apps so that the next retry can start afresh
+        mDevice.pressHome();
+        mDevice.pressRecentApps();
+
+        UiObject apps = mDevice.findObject(new UiSelector().resourceId("android:id/content"));
+        mDevice.drag(apps.getBounds().left,
+                apps.getBounds().centerY(),
+                apps.getBounds().right,
+                apps.getBounds().centerY(),
+                10);
+
+        UiObject clearButton = mDevice.findObject(new UiSelector().text("Clear all"));
+        if ( clearButton.waitForExists(5)) clearButton.click();
+
+        Log.i("Framework", "Clear recently opened apps");
+        mDevice.pressHome();
+    }
+
+    @Override
+    public Statement apply(final Statement base, final Description description) {
+        return statement(base, description);
+    }
+
+    private Statement statement (final Statement base, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                Throwable throwable = null;
+
+                mDevice.wakeUp();
+                Assert.assertTrue("Failed to wake up the device.", mDevice.isScreenOn());
+                // Press "Menu" to unlock screen if any.
+                mDevice.pressMenu();
+                // Reset device to dismiss a lock screen if any.
+                resetDeviceState();
+
+                // Implement retry logic here
+                for (int i = 0; i < RETRY_COUNT; i++) {
+                    Log.i("Framework", "Try " + i);
+                    throwable = null;
+                    try {
+                        base.evaluate();
+                    } catch (Throwable t) {
+                        throwable = t;
+                        if ( i == RETRY_COUNT-1 ) {
+                            File loggingDir = getLoggingDir(description.getTestClass().getSimpleName(),
+                                                            description.getMethodName());
+
+                            // Capture the window UI hierarchy when a test fails.
+                            mDevice.dumpWindowHierarchy(new File(loggingDir, "hierarchy_1.xml"));
+
+                            // Snap the screenshot when a test fails.
+                            mDevice.takeScreenshot(new File(loggingDir, "screenshot_1.png"));
+
+                            // Log the error message
+                            PrintWriter error =
+                                    new PrintWriter(new File(loggingDir, "error.txt").getPath(), "UTF-8");
+                            t.printStackTrace(error);
+                            error.close();
+                        }
+                        // clear all recently opened apps if any before retry
+                        resetDeviceState();
+                    }
+                    if (throwable == null) {
+                        return;
+                    }
+                }
+                throw throwable;
+            }
+        };
+    }
+}
