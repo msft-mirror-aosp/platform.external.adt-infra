edfbd0a01e159e7cde2b3326c94cdcf1d6166871
Akash Agrawal <akagrawal@google.com>
2020-10-23 10:33:58 -0700

    Fix CameraTest for phone capture and video capture in API 29.
    Refactored camera mode labels and selectors to produce matches. Added an assert for deleted files to ensure the deleted process is completed.
    
    Change-Id: I8b40d6c912332a3a482edd259e8bdf2e0ff0f538

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api29/CameraTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api29/CameraTest.java
@@ -76,7 +76,7 @@ public class CameraTest {
     public void testPhotoCapture() throws Exception {
         Instrumentation instrumentation = testFramework.getInstrumentation();
 
-        boolean photoTestSuccess = CameraTestUtil.useCamera(instrumentation, "Camera", 2);
+        boolean photoTestSuccess = CameraTestUtil.useCamera(instrumentation, "Images", 2);
         Assert.assertTrue("New photo was not deleted from the gallery", photoTestSuccess);
     }
 
