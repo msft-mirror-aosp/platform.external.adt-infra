3adb2b80b1e3638e4a21d349eecdeec8489e883b
Erwin Jansen <jansene@google.com>
2021-01-06 14:45:40 -0800

    [gRPC] e2e tests for touch events.
    Used to verify that events with various ids work.
    
    Bug: b/176751160
    Change-Id: I6d06eb5f1a5e77c9516a106e6481f837de087f74

---
+++ emu_test/test_embedded/tests/test_touch_event.py
@@ -0,0 +1,58 @@
+# Copyright 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pytest
+from aemu.proto.emulator_controller_pb2 import Touch, TouchEvent
+
+
+@pytest.mark.e2e
+@pytest.mark.timeout(timeout=10, func_only=True)
+def test_touch_event_identifier_ranges(at_home):
+    """Tests that we properly handle identifiers."""
+    stub = pytest.emulator.get_emulator_controller()
+    stub.sendTouch(
+        TouchEvent(touches=[Touch(x=661, y=1133, pressure=12, identifier=-23122)])
+    )
+    stub.sendTouch(TouchEvent(touches=[Touch(x=661, y=1133, pressure=1)]))
+    stub.sendTouch(
+        TouchEvent(touches=[Touch(x=335, y=940, identifier=-124543, pressure=1)])
+    )
+    stub.sendTouch(TouchEvent(touches=[Touch(x=203, y=823, identifier=0)]))
+    stub.sendTouch(TouchEvent(touches=[Touch(x=203, y=823, identifier=-124543)]))
+    stub.sendTouch(TouchEvent(touches=[Touch(x=801, y=1281, identifier=-23122)]))
+
+
+@pytest.mark.e2e
+@pytest.mark.timeout(timeout=10, func_only=True)
+def test_touch_event_identifier_to_many(at_home):
+    """Tests that we properly handle too many registered identifiers."""
+    stub = pytest.emulator.get_emulator_controller()
+    x = 1
+    for j in range(20):
+        for i in range(10):
+            stub.sendTouch(
+                TouchEvent(
+                    touches=[
+                        Touch(x=i * 10, y=i * 10, pressure=12, identifier=j * 10 + i)
+                    ]
+                )
+            )
+
+        for i in range(10):
+            stub.sendTouch(
+                TouchEvent(
+                    touches=[
+                        Touch(x=i * 10, y=i * 10, pressure=0, identifier=j * 10 + i)
+                    ]
+                )
+            )
