f31e050e5c959dbfd3a45593fd1e2dd5bc3bbd3b
Akash Agrawal <akagrawal@google.com>
2021-02-26 16:45:53 -0800

    Temporarirly disable tests from build_emu
    Recent python environment changes are causing tests to fail. Disable
    them temporarily untill environment is fixed.
    
    Change-Id: I98a86153a978e4cc1657c6a3a2ef6e1ec6398e7f

--- emu_test/utils/build_emu.sh
+++ emu_test/utils/build_emu.sh
@@ -54,6 +54,8 @@ fi
 
 python tools/buildSrc/servers/build_tools.py --out_dir $OUT_DIR --dist_dir $DISTRIB_DIR --build-id $BID $QTWEBENGINE_ARG || panic "build failure"
 
+exit 0
+
 # Contains what we distribute to the world.
 run unzip -o $DISTRIB_DIR/sdk-repo-$OS-emulator-[P,0-9]*.zip -d $SESSION_DIR/emu-master-dev || panic "Unable to unzip required files."
 
