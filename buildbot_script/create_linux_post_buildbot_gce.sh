ce2f3fd8bdbae3d44aa2ecc0cf0a9f078e2f56a2
Akash Agrawal <akagrawal@google.com>
2021-03-18 17:04:56 -0700

    Update buildbot image version
    Buildbot image is updated with windows 10 sdk toolchain
    
    Change-Id: Ieae3adfe0465080f691bfae5569f7f8974200a2f

--- buildbot_script/create_linux_post_buildbot_gce.sh
+++ buildbot_script/create_linux_post_buildbot_gce.sh
@@ -15,15 +15,15 @@
 # adt-emu-buildbot-6-prod   Added darwin cross compiler
 # adt-emu-buildbot-7-test   1. Upgrade to ubuntu18
 #                           2. Added toolchain for arc64
-#                            (does not start buildbot)
 # adt-emu-buildbot-7-prod   1. Upgrade to ubuntu18
 #                           2. Added toolchain for arc64
-#                            (starts buildbot on startup)
 # adt-emu-buildbot-10-prod  Git version 2.29
 #                            Upgrade SDK 10.15 cross buildchain
-#                            (does not start buildbot)
 # adt-emu-buildbot-10-test  Git version 2.29
 #                            Upgrade SDK 10.15 cross buildchain
+# adt-emu-buildbot-11-test  Windows 10 sdk toolchain update
+#                            (does not start buildbot)
+# adt-emu-buildbot-11-prod  Windows 10 sdk toolchain update
 #                            (starts buildbot on startup)
 
 if [ $# -ne 2 ]
@@ -46,7 +46,7 @@ then
    export IMAGE_TYPE="prod"
 fi
 
-export IMAGE_NAME=adt-emu-buildbot-10-$IMAGE_TYPE
+export IMAGE_NAME=adt-emu-buildbot-11-$IMAGE_TYPE
 export TEMP_INSTANCE=adt-emu-buildbot-$1
 export PROJECT=android-studio-build
 export FULL_PROJECT=android-studio-build
