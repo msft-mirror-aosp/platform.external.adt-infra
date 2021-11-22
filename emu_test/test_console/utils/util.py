84aa7f800fd7390575f6a79de06c032764a73e20
David Yu <dyu@google.com>
2021-11-22 18:50:10 +0000

    Disable boot tests on Mac.
    Boot tests failing on Mac. Disable tests.
    
    BUG: b/207382563
    Change-Id: Ie636192b3f7a7277631d41ffa7596eecb4a1dc31

--- emu_test/test_console/utils/util.py
+++ emu_test/test_console/utils/util.py
@@ -29,6 +29,12 @@ if os.name == WINDOWS_OS_NAME:
 else:
   COMPARE_CMD = 'diff'
 
+MAC_OS_NAME = 'posix'
+if os.name == MAC_OS_NAME:
+  COMPARE_CMD = 'FC'
+else:
+  COMPARE_CMD = 'diff'
+
 SERVER_NAME = 'localhost'
 CONSOLE_PORT = 5554
 
