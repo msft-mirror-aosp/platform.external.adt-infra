d8ef89be54aac5e4121c207b78465ebef219682b
David Yu <dyu@google.com>
2021-12-10 00:31:05 +0000

    Separate test_help_command to divide auth and noauth tests.
    Separate tests to avoid flakiness on some bots during run.
    
    Change-Id: Id358840fdd49e35d03a03ed425b3fefccfe92eff

--- emu_test/test_console/testcase_help.py
+++ emu_test/test_console/testcase_help.py
@@ -69,7 +69,7 @@ class HelpTest(testcase_base.BaseConsoleTest):
         self.telnet.read_until(util.toBytes(util.OK), util.TIMEOUT_S))):
       sys.exit(-1)
 
-  def test_help_command(self):
+  def test_help_command_noauth(self):
     """Test command for: help.
 
     TT ID: b4bed6f1-062d-4a52-b8c2-b9eb0c445ab0
@@ -77,20 +77,28 @@ class HelpTest(testcase_base.BaseConsoleTest):
       1. Launch an emulator avd
       2. From command prompt, run: telnet localhost <port>
       3. Run: help, and verify 1
-      4. Copy the auth_token value from ~/.emulator_console_auth_token
-      5. Run: auth auth_token
-      6. Run: help, and verify 2
     Verify:
       1. help, auth, avd and quit/exit commands are available
-      2. crash, kill, redir, power, event, avd ,finger, geo, sms, cdma,
-         gsm and rotate commands are available
     """
     print(('Running test: %s' % (inspect.stack()[0][3])))
     self._help_command(util.CMDS_FOR_HELP_NO_AUTH)
     self._help_verbose_command(util.CMDS_FOR_HELP_VERBOSE_NO_AUTH)
 
-    self._auth_user_for_emulator_console()
+  def test_help_command_auth(self):
+    """Test command for: help.
 
+    TT ID: b4bed6f1-062d-4a52-b8c2-b9eb0c445ab0
+    Test steps:
+      1. Launch an emulator avd
+      2. Copy the auth_token value from ~/.emulator_console_auth_token
+      3. Run: auth auth_token
+      4. Run: help, and verify 1
+    Verify:
+      1. crash, kill, redir, power, event, avd ,finger, geo, sms, cdma,
+         gsm and rotate commands are available
+    """
+    print(('Running test: %s' % (inspect.stack()[0][3])))
+    self._auth_user_for_emulator_console()
     self._help_command(util.CMDS_FOR_HELP_AUTH)
     self._help_verbose_command(util.CMDS_FOR_HELP_VERBOSE_DISPLAY_AUTH)
 
