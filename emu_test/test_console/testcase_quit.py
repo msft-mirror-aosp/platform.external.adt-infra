e937c114eabef78fb6dabfbe9a371affdc1ef292
Pallav Johari <pjohari@google.com>
2022-02-09 21:56:23 -0800

    Unskipping test cases for b/215910720, b/205143919
    Change-Id: Idfcb74158f8b05713bb43e5f4840795fecc0fd89

--- emu_test/test_console/testcase_quit.py
+++ emu_test/test_console/testcase_quit.py
@@ -44,7 +44,6 @@ class QuitTest(testcase_base.BaseConsoleTest):
         is_command_successful, 'Failed to properly quit/exit emulator.',
         False, '', EMPTY_OUTPUT, output_exit)
 
-  @unittest.skipIf(os.name == LINUX_MAC_OS_NAME, "b/215910720")
   def test_quit_command(self):
     """Test command for: quit.
 
@@ -62,7 +61,6 @@ class QuitTest(testcase_base.BaseConsoleTest):
     print(('Running test: %s' % (inspect.stack()[0][3])))
     self._execute_command_and_verify(CMD_QUIT)
 
-  @unittest.skipIf(os.name == LINUX_MAC_OS_NAME, "b/215910720")
   def test_exit_command(self):
     """Test command for: exit.
 
