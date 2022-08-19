a09c650a07c264e7ad0f60dd2b4b5db892426b69
Huan Song <huans@google.com>
2022-08-19 13:48:18 -0700

    Improve the resizable e2e test
    run the Animation app and make sure the red square is at the proper
    position of the display.
    
    Test: pytest --debug_emulator_log=/tmp/emu.log --debug_emulator -k test_resizable
    
    Change-Id: I088831fe56fab651eb9024ae314fffcc931cfe71

--- pytest/test_embedded/tests/resizable/test_resizable.py
+++ pytest/test_embedded/tests/resizable/test_resizable.py
@@ -24,13 +24,12 @@ from aemu.proto.emulator_controller_pb2 import (
 )
 from PIL import Image
 _EMPTY_ = empty_pb2.Empty()
-_DIFF_MAX_ = 0.01
 
 avd_config = {
     "api": "33",
     "tag.id": "google_apis",
     "hw.device.name": "resizable",
-    "hw.resizable.configs": "phone-0-1080-2340-420, foldable-1-1768-2208-420, tablet-2-1920-1200-240, desktop-3-1920-1080-160",
+    "hw.resizable.configs": "phone-0-1080-2340-420, foldable-1-1768-2208-420, tablet-2-1920-1200-24 0",
     "skin.name": "1080x2340",
     "skin.path": "no_skin",
 }
@@ -42,30 +41,38 @@ def set_device_display_mode(emu, mode):
             value=mode,
         )
     )
-    time.sleep(5)
+    time.sleep(10)
 
-def img_compare(width, height, img, expected_img_file):
-    current_path = os.path.dirname(os.path.realpath(__file__))
-    filename = os.path.join(current_path, expected_img_file)
-    expected_img = Image.open(filename).convert('RGB')
+def img_compare(width, height, img, left, right, top, bottom):
     actual_img = Image.frombytes(
         "RGB", (width, height), img.image
     )
+    found_first = False
+    l = 0
+    r = 0
+    t = 0
+    b = 0
+    for h in range(height):
+      for w in range(width):
+        if actual_img.getpixel((w, h)) == (255, 0, 0):
+          if not found_first:
+            found_first = True
+            l = w
+            t = h
+          else:
+            r = w
+            b = h
+    print('expect', left, right, top, bottom, 'found', l, r, t, b)
+    return left == l and right == r and top == t and bottom == b
 
-    different_pixels = 0
-    for w in range(width):
-      for h in range(height):
-        if actual_img.getpixel((w, h)) != expected_img.getpixel((w, h)):
-          different_pixels+=1
-    return different_pixels/(width*height)
-
-@pytest.mark.parametrize("w,h,mode,expected_image",
-                         [(1080, 2340, DisplayModeValue.PHONE, "phone.png"),
-                          (1768, 2208, DisplayModeValue.FOLDABLE, "foldable.png"),
-                          (1920, 1200, DisplayModeValue.TABLET, "tablet.png"),
-                          (1920, 1080, DisplayModeValue.DESKTOP, "desktop.png")])
-def test_resizable(emulator_controller, w, h, mode, expected_image):
-    """test PHONE mode"""
+"""Run Animation app. Take a screenshot. Locate the four edges of the red square,
+   and make sure they are at the proper position of the display"""
+@pytest.mark.parametrize("w, h, mode, left, right, top, bottom",
+                         [(1080, 2340, DisplayModeValue.PHONE, 882, 1079, 0, 227),
+                          (1768, 2208, DisplayModeValue.FOLDABLE, 1191, 1767, 0, 204),
+                          (1920, 1200, DisplayModeValue.TABLET, 1127, 1514, 0, 110)])
+def test_resizable(animation_app, emulator_controller, w, h, mode,
+                   left, right, top, bottom):
     set_device_display_mode(emulator_controller, mode)
     image = emulator_controller.getScreenshot(
         ImageFormat(
@@ -75,5 +82,5 @@ def test_resizable(emulator_controller, w, h, mode, expected_image):
     assert emulator_controller.getDisplayMode(_EMPTY_).value == mode
     assert image.format.width == w
     assert image.format.height == h
-    diff = img_compare(image.format.width, image.format.height, image, expected_image)
-    assert diff < _DIFF_MAX_
+    assert img_compare(image.format.width, image.format.height, image,
+                       left, right, top, bottom)
