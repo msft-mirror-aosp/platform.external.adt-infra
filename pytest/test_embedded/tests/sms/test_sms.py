220d70617c5b46d6e16ef91c9a29ffcd982f3985
David Yu <dyu@google.com>
2022-09-29 19:25:35 -0700

    Add sms test in pytest.
    Test sends inbound phone number and test message. Test verifies the text
    message is received from the phone number.
    
    BUG: b/249871970
    Change-Id: I09d0791fe292617cb8073d7498be382576a599ff

---
+++ pytest/test_embedded/tests/sms/test_sms.py
@@ -0,0 +1,41 @@
+# Copyright 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from aemu.proto.emulator_controller_pb2 import (
+    SmsMessage
+)
+
+@pytest.mark.e2e
+@pytest.mark.timeout(timeout=10, func_only=True)
+@pytest.mark.parametrize(
+    "phone_number,text_message", [('987654321', 'Hello There')]
+)
+def test_send_inbound_sms_text_message(emulator_controller, phone_number, text_message):
+    """Send inbound phone number and text message.
+
+    Verify text message is received from phone number.
+
+    Args:
+      phone_number: phone number text message is sent from.
+      text_message: contents of text message.
+    """
+
+    message = SmsMessage(
+        srcAddress = phone_number,
+        text = text_message
+    )
+    response = emulator_controller.sendSms(message)
+    assert response.response == response.OK
