81f56479c5fd0268982f3456135a693e32da4581
Ranjit Devarasetty <ranjitkumar@google.com>
2022-10-14 14:17:16 -0700

    Add clipboard test to pytest framework.
    This test sets the clipboard data to emulator then gets the
    clipboard data and verifies.
    
    Bug: 250955568
    Change-Id: I5934e7d55f6ae7f34f4a673cc0ca95983d666d3e

---
+++ pytest/test_embedded/tests/clipboard/test_clipboard.py
@@ -0,0 +1,47 @@
+# Copyright 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from aemu.proto.emulator_controller_pb2 import (
+    ClipData
+)
+
+from google.protobuf import empty_pb2
+
+_EMPTY_ = empty_pb2.Empty()
+
+@pytest.mark.e2e
+@pytest.mark.timeout(timeout=10, func_only=True)
+@pytest.mark.parametrize("clipboard_data",
+                         [
+                             'Hello There!',
+                             'How is Weather?',
+                         ]
+)
+def test_clipboard_data(emulator_controller, clipboard_data):
+    """Send clipboard data to the emulator.
+
+    Verify that the clipboard data received is same.
+
+    Args:
+      clipboard_data: clipboard data to be set.
+    """
+    set_clip_data = ClipData(
+        text = clipboard_data
+    )
+    emulator_controller.setClipboard(set_clip_data)
+
+    get_clip_data = emulator_controller.getClipboard(_EMPTY_)
+    assert set_clip_data.text == get_clip_data.text, "Clipboard data doesn't match"
