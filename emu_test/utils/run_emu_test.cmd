6827926ad64561b90d3c1ec8ee023949c9abff36
Erwin Jansen <jansene@google.com>
2023-02-14 11:12:21 -0800

    Fix incorrect logdir in windows
    This updates the logging directory to be the same as on linux/mac.
    This will make sure the bots will automatically pick up the test
    results.
    
    Change-Id: Id2fc9347c17f913b870c88c74a445ae16b442d23

--- emu_test/utils/run_emu_test.cmd
+++ emu_test/utils/run_emu_test.cmd
@@ -6,7 +6,7 @@ REM This will be invoked by aosp-emu-master-dev.
 
 set SESSION_DIR=%1
 
-prebuilts\python\windows-x86\python.exe external\adt-infra\pytest\test_embedded\run_tests.py --build_dir out\prebuilt_cached\builds --logdir %SESSION_DIR%\logdir
+prebuilts\python\windows-x86\python.exe external\adt-infra\pytest\test_embedded\run_tests.py --build_dir out\prebuilt_cached\builds --logdir %SESSION_DIR%\testlogs
 if errorlevel 1 (
     echo "Failures during test execution!"
     exit /b 1
