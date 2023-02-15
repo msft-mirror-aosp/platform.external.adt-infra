7dac24cf028a779b411d994155b0ae9ff12f8631
Erwin Jansen <jansene@google.com>
2023-02-15 16:18:00 +0000

    Revert "Fix windows test break"
    This reverts commit 379edaad10ae4d1acf72f8f82e9139988975c858.
    
    Reason for revert: Somehow this makes matters worse and locks up the build bots.
    
    Change-Id: I148ba5f7dc4c6dc9ee4d729fc5727790f2e878cf

--- pytest/test_embedded/run_tests.py
+++ pytest/test_embedded/run_tests.py
@@ -197,7 +197,7 @@ class TemporaryEmulatorDeploy:
 
     def __init__(self, build_dir):
         self.build_dir = Path(build_dir)
-        self.tmp = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
+        self.tmp = tempfile.TemporaryDirectory()
 
         if not self.build_dir.exists():
             raise BuildDirectoryNotFound(
@@ -407,7 +407,7 @@ class AospPyRunner(PyRunner):
             )
             self.py_exe = PYTHON
         else:
-            self.tmp = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
+            self.tmp = tempfile.TemporaryDirectory()
             tmpdir = Path(self.tmp.name)
             run(
                 [
@@ -520,7 +520,7 @@ def run_tests(
 
     logdir = Path(logdir) / "embedded_test" / "log"
     logdir.mkdir(exist_ok=True, parents=True)
-    with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as tmpdir:
+    with tempfile.TemporaryDirectory() as tmpdir:
         junit_test_results = Path(tmpdir) / "test_unit.xml"
         try:
             pyrun.run(
