e338b74403dc06448c67318bf0a42c3dcb1c04da
Bo Hu <bohu@google.com>
2023-03-16 13:29:23 -0700

    test: skip some rotation tests for now
    They are buggy and need to be fixed first
    
    Change-Id: Ia0f0257e10343c2c66dab1173944bd499b494543

--- pytest/test_embedded/tests/display/test_rotation.py
+++ pytest/test_embedded/tests/display/test_rotation.py
@@ -277,6 +277,7 @@ def test_rotation_pixels_in_the_right_place(
 
 @pytest.mark.e2e
 @pytest.mark.flaky(reruns=2, reruns_delay=2)
+@pytest.mark.skip(reason="real bug with adb emu rotate, need to fix it first")
 def test_rotation_through_console_observable_through_physical_model(
     emulator_controller, adb, at_home
 ):
@@ -298,6 +299,7 @@ def test_rotation_through_console_observable_through_physical_model(
 
 @pytest.mark.e2e
 @pytest.mark.timeout(timeout=10, func_only=True)
+@pytest.mark.skip(reason="real bug with adb emu rotate, need to fix it first")
 def test_rotation_through_console_observable_through_screenshot(
     at_home, emulator_controller, adb
 ):
@@ -309,6 +311,7 @@ def test_rotation_through_console_observable_through_screenshot(
 
 @pytest.mark.e2e
 @pytest.mark.timeout(timeout=10, func_only=True)
+@pytest.mark.skip(reason="real bug with adb emu rotate, need to fix it first")
 def test_rotation_through_console_observable_through_stream_screenshot(
     at_home, animation_app, emulator_controller, adb
 ):
@@ -339,6 +342,7 @@ def test_rotation_observable_through_screenshot_embedded_mode(
 @pytest.mark.embedded
 @pytest.mark.timeout(timeout=480, func_only=True)
 @pytest.mark.flaky(reruns=2, reruns_delay=2)
+@pytest.mark.skip(reason="real bug with adb emu rotate, need to fix it first")
 @pytest.mark.parametrize("avd", ["-qt-hide-window"], indirect=True)
 def test_rotation_observable_through_stream_screenshot_embedded_mode(
     emulator_controller, adb, emulator
