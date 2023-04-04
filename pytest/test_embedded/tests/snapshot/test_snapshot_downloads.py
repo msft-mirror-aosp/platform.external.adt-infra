5d51ac48deff0521b87c948821506c8d593fef94
Bo Hu <bohu@google.com>
2023-04-04 00:47:37 +0000

    Revert "test: use prebooted s pixel2 homescreen png to check boot complete"
    This reverts commit eacfd5ba4e993ee7c5c5f4ea50d00a751d26062c.
    
    Reason for revert: just test
    
    Change-Id: I91b3266fc6e813732723c9cef78d8f40a0d831e2

--- pytest/test_embedded/tests/snapshot/test_snapshot_downloads.py
+++ pytest/test_embedded/tests/snapshot/test_snapshot_downloads.py
@@ -129,7 +129,6 @@ def test_can_load_oldsnapshot(emulator):
     emulator.exe = oldexe;
     assert emulator.launch(flags=["-no-snapshot-load"])
     assert emulator.wait_for_boot(timeout=420)
-    assert emulator.wait_for_homescreen()
     # there is no reliable way to detect it has reach home screen
     # so just wait enough long
     time.sleep(10)
