906e059b1aa0e74c0a4b23b6073800e362d47d6e
Ranjit Devarasetty <ranjitkumar@google.com>
2023-04-18 12:12:51 -0700

    Added test to validate the first and second level files and folders under emulator root directory are as expected.
    Bug: 265306494
    Change-Id: I6d23247e03529ef13a5678b7208e900153db8473

---
+++ pytest/test_embedded/tests/files/test_zip_distribution.py
@@ -0,0 +1,292 @@
+import os
+import platform
+import pytest
+
+from pathlib import Path
+from emu.emulator import BaseEmulator
+
+
+@pytest.mark.e2e
+@pytest.mark.timeout(timeout=20, func_only=True)
+def test_zip_file_contains_all_expected_files(emulator: BaseEmulator):
+    emu_root = Path(emulator.exe).parent
+    assert emu_root.exists(), "Emulator folder does not exist."
+
+    act_files = [f.name for f in os.scandir(emu_root) if f.is_file()]
+    assert len(act_files) > 0, f"{emu_root} folder should not be empty."
+    for file in get_expected_files():
+        assert file in act_files, f"{file} is missing in the Emulator directory."
+
+    act_sub_dirs = [f.name for f in os.scandir(emu_root) if f.is_dir()]
+    assert len(act_sub_dirs) > 0, f"{emu_root} folder missing sub directories."
+    exp_sub_dirs = get_exp_sub_dirs()
+    for folder in exp_sub_dirs:
+        assert (
+            folder in act_sub_dirs
+        ), f"{folder} is missing in the Emulator directory."
+        sub_dir_files = [f.name for f in os.scandir(os.path.join(emu_root, folder))]
+        for file in exp_sub_dirs[folder]:
+            assert (
+                file in sub_dir_files
+            ), f"{file} is missing in the Emulator sub directory {folder}."
+
+
+def get_expected_files():
+    """ given a OS environment return the expected files under emulator folder.
+
+    """
+    exp_files = [
+        "NOTICE.csv",
+        "qsn",
+        "qemu-img",
+        "crashreport",
+        "LICENSE",
+        "crashpad_handler",
+        "source.properties",
+        "emulator-check",
+        "NOTICE.txt",
+        "netsimd",
+        "android-info.txt",
+        "mksdcard",
+        "nimble_bridge",
+        "emulator",
+        "netsim"
+    ]
+
+    exp_files_win = [
+        "NOTICE.csv",
+        "qsn.exe",
+        "qemu-img.exe",
+        "crashreport.exe",
+        "LICENSE",
+        "crashpad_handler.exe",
+        "source.properties",
+        "emulator-check.exe",
+        "NOTICE.txt",
+        "netsimd.exe",
+        "android-info.txt",
+        "mksdcard.exe",
+        "nimble_bridge.exe",
+        "emulator.exe",
+        "netsim.exe",
+        "android-emu-agents.lib",
+        "concrt140.dll",
+        "libandroid-emu-agents.dll",
+        "libprotobuf.dll",
+        "msvcp140.dll",
+        "msvcp140_1.dll",
+        "msvcp140_2.dll",
+        "msvcp140_atomic_wait.dll",
+        "msvcp140_codecvt_ids.dll",
+        "protobuf.lib",
+        "vccorlib140.dll",
+        "vcruntime140.dll",
+        "vcruntime140_1.dll"
+    ]
+
+    if platform.system() == "Windows":
+            return exp_files_win
+    elif platform.system() == "Linux" or platform.system() == "Darwin":
+            return exp_files
+
+def get_exp_sub_dirs():
+    """ given a OS environment return the expected sub directories and
+        files under them.
+
+    """
+    exp_sub_dirs = {
+        "bin64": ["e2fsck", "fsck.ext4", "mkfs.ext4", "resize2fs", "tune2fs"],
+        "include": ["flatbuffers"],
+        "lib": [
+            "adb_service.proto",
+            "emu-original-feature-flags.protobuf",
+            "emulator_controller.proto",
+            "pc-bios",
+            "advancedFeatures.ini",
+            "emulated_bluetooth.proto",
+            "emulator_stats.proto",
+            "pkgconfig",
+            "advancedFeaturesCanary.ini",
+            "emulated_bluetooth_device.proto",
+            "grpc_endpoint_description.proto",
+            "snapshot.proto",
+            "ca-bundle.pem",
+            "emulated_bluetooth_packets.proto",
+            "hardware-properties.ini",
+            "snapshot_service.proto",
+            "cmake",
+            "emulated_bluetooth_vhci.proto",
+            "hostapd.conf",
+            "ui_controller_service.proto",
+            "control_socket.proto",
+            "emulator_access.json",
+            "libflatbuffers.a",
+            "waterfall.proto"
+        ],
+        "lib64": [
+            "gles_angle",
+            "libandroid-emu-protos.dylib",
+            "libgfxstream_backend.dylib",
+            "libprotobuf.dylib",
+            "libandroid-emu-agents.dylib",
+            "libandroid-emu-shared.dylib",
+            "libimage_converter.dylib",
+            "libshadertranslator.dylib",
+            "libandroid-emu-curl.dylib",
+            "libandroid-emu-tracing.dylib",
+            "libprotobuf.3.21.12.0.dylib",
+            "qt",
+            "libandroid-emu-metrics.dylib",
+            "libemugl_common.dylib",
+            "libprotobuf.32.dylib",
+            "vulkan"
+        ],
+        "netsim-ui": ["assets", "index.html", "js", "node_modules"],
+        "resources": [
+            "Toren1BD.mtl",
+            "Toren1BD.posters",
+            "Toren1BD_Main.png",
+            "macros",
+            "Toren1BD.obj",
+            "Toren1BD_Decor.png",
+            "macroPreviews",
+            "poster.png"
+        ]
+    }
+
+    exp_sub_dirs_win = {
+        "bin64": [
+            "e2fsck.exe",
+            "resize2fs.exe",
+            "tune2fs.exe"
+            "cygblkid-1.dll",
+            "cygcom_err-2.dll",
+            "cyge2p-2.dll",
+            "cygext2fs-2.dll",
+            "cyggcc_s-1.dll",
+            "cygiconv-2.dll",
+            "cygintl-8.dll",
+            "cyguuid-1.dll",
+            "cygwin1.dll"
+        ],
+        "include": ["flatbuffers"],
+        "lib": [
+            "adb_service.proto",
+            "emu-original-feature-flags.protobuf",
+            "emulator_controller.proto",
+            "pc-bios",
+            "advancedFeatures.ini",
+            "emulated_bluetooth.proto",
+            "emulator_stats.proto",
+            "pkgconfig",
+            "advancedFeaturesCanary.ini",
+            "emulated_bluetooth_device.proto",
+            "grpc_endpoint_description.proto",
+            "snapshot.proto",
+            "ca-bundle.pem",
+            "emulated_bluetooth_packets.proto",
+            "hardware-properties.ini",
+            "snapshot_service.proto",
+            "cmake",
+            "emulated_bluetooth_vhci.proto",
+            "hostapd.conf",
+            "ui_controller_service.proto",
+            "control_socket.proto",
+            "emulator_access.json",
+            "waterfall.proto",
+            "flatbuffers.lib"
+        ],
+        "lib64": [
+            "gles_angle",
+            "libandroid-emu-protos.dll",
+            "libgfxstream_backend.dll",
+            "libprotobuf.dll",
+            "libandroid-emu-agents.dll",
+            "libandroid-emu-shared.dll",
+            "libimage_converter.dll",
+            "libshadertranslator.dll",
+            "libandroid-emu-curl.dll",
+            "libandroid-emu-tracing.dll",
+            "qt",
+            "libandroid-emu-metrics.dll",
+            "libemugl_common.dll",
+            "vulkan",
+            "vcruntime140_1.dll",
+            "vcruntime140.dll",
+            "vccorlib140.dll",
+            "msvcp140_codecvt_ids.dll",
+            "msvcp140_atomic_wait.dll",
+            "msvcp140_2.dll",
+            "msvcp140_1.dll",
+            "msvcp140.dll",
+            "glib-2-vs11.dll",
+            "gles_swiftshader",
+            "gles_mesa",
+            "concrt140.dll"
+        ],
+        "netsim-ui": ["assets", "index.html", "js", "node_modules"],
+        "resources": [
+            "Toren1BD.mtl",
+            "Toren1BD.posters",
+            "Toren1BD_Main.png",
+            "macros",
+            "Toren1BD.obj",
+            "Toren1BD_Decor.png",
+            "macroPreviews",
+            "poster.png"
+        ],
+        "drivers": [
+            "UsbAssist_Install.bat",
+            "UsbAssist.sys",
+            "UsbAssist.inf",
+            "usbassist.cat",
+            "Install_Drivers.bat",
+            "Android_USB_Assistant_Install.bat",
+            "Android_USB_Assistant.inf",
+            "Android_USB_Assistant.cat"
+        ]
+    }
+
+    lib64_linux = [
+        "gles_angle",
+        "gles_mesa",
+        "gles_swiftshader",
+        "libandroid-emu-protos.so",
+        "libgfxstream_backend.so",
+        "libprotobuf.so",
+        "libandroid-emu-agents.so",
+        "libandroid-emu-shared.so",
+        "libimage_converter.so",
+        "libshadertranslator.so",
+        "libandroid-emu-curl.so",
+        "libandroid-emu-tracing.so",
+        "libprotobuf.so.3.21.12.0",
+        "qt",
+        "libandroid-emu-metrics.so",
+        "libemugl_common.so",
+        "libprotobuf.so.32",
+        "vulkan",
+        "libandroid-webrtc.so",
+        "libc++.so",
+        "libc++.so.1",
+        "libtcmalloc_minimal.so.4"
+    ]
+
+    if platform.system() == "Windows":
+        exp_sub_dirs_win["qemu"] = ["windows-x86_64"]
+        return exp_sub_dirs
+    elif platform.system() == "Linux":
+        exp_sub_dirs["qemu"] = ["linux-x86_64"]
+        exp_sub_dirs["lib"].append("ice_config.proto")
+        exp_sub_dirs["lib"].append("rtc_service.proto")
+        exp_sub_dirs["lib"].append("rtc_service_v2.proto")
+        exp_sub_dirs["lib64"].update(lib64_linux)
+        return exp_sub_dirs
+    elif platform.system() == "Darwin":
+        if platform.processor() == "arm":
+            exp_sub_dirs["qemu"] = ["darwin-aarch64"]
+            exp_sub_dirs["_CodeSignature"] = ["CodeResources"]
+            return exp_sub_dirs
+        else:
+            exp_sub_dirs["qemu"] = ["darwin-x86_64"]
+            return exp_sub_dirs
