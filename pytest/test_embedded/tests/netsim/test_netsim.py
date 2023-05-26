6c869cd78f9f093f8a0a9f3c173173b05308603d
Erwin Jansen <jansene@google.com>
2023-05-26 11:06:36 -0700

    Add E2E that checks that netsimd is launched
    We expect netsimd to be launched as part of the boot process. Once
    android has completed booting we expect the bluetooth module to be
    active, which means netsimd should be running
    
    Bug: 284486462
    Change-Id: I6e4fea9750d590da6c680ba282a59c92bee7926e

---
+++ pytest/test_embedded/tests/netsim/test_netsim.py
@@ -0,0 +1,14 @@
+import psutil
+import pytest
+from emu.timing import eventually
+
+
+@pytest.mark.e2e
+@pytest.mark.timeout(timeout=20, func_only=True)
+def test_netsimd_is_launched(avd):
+    """Test case to verify that the 'netsimd' process is launched."""
+    assert eventually(
+        lambda: any(
+            "netsimd" in process.name() for process in psutil.process_iter(["name"])
+        )
+    )
