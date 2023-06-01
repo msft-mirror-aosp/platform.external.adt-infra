34f6eba8e2997f33701ed5444c6be8bcf60a4ed9
Erwin Jansen <jansene@google.com>
2023-05-31 17:10:38 -0700

    Add e2e test validating input events
    This adds an e2e test that verifies that the animation app is receiving
    all the expected events we send via gRPC.
    
    Note this approach is a bit limited:
    
    - We can only detect a combination of up/down events
    - We have to do some rate limiting
    
    Since we have -debug-events enabled again we re-enable the tests
    based on validating the arrival of events.
    
    The animation app will now mark itself as started once it has rendered
    a series of frames.
    
    Note: This upgrades the apk to be signed, as we can no longer check in
    debug apks.
    
    Bug: 245964307
    Change-Id: I53a7d895cb637127dd922742d3640785b614ceee

---
+++ pytest/test_embedded/tests/events/test_input_events_in_animation.py
@@ -0,0 +1,175 @@
+# Copyright 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+import time
+import logging
+
+import pytest
+from aemu.proto.emulator_controller_pb2 import MouseEvent, KeyboardEvent, InputEvent
+
+
+@pytest.fixture
+def retrieve_events(avd):
+    """Retrieves events from the aemu animation app.
+
+    The animation app logs key and mouse events. With this
+    fixture you can extract them after you have send your
+    sequence of events.
+
+    expected = [....]
+    send_events(...)
+
+    assert expected == retrieve_events()
+
+    Note: This will pull everything from the adb stream and wait at 1s before it returns..
+
+    Returns:
+        A list of events.
+    """
+
+    def _extract_event(line: str):
+        """Extracts an event from a line of logcat output.
+
+        Args:
+            line: A line of logcat output.
+
+        Returns:
+            An event, or an empty dictionary if the line does not contain an event.
+        """
+        AEMU_STR = "I aemu    :"
+        try:
+            offset = line.index(AEMU_STR) + len(AEMU_STR)
+            return json.loads(line[offset:])
+        except ValueError as _ignore:
+            return {}
+
+    def _retrieve_events():
+        """Retrieves events from logcat.
+
+        Returns:
+            A list of events.
+        """
+        observed = []
+        with avd.adb.logcat(tag="aemu", timeout=1) as stream:
+            for logline in stream:
+                logging.info("Extracting %s", logline)
+                event = _extract_event(logline)
+                if "type" in event:
+                    observed.append(event)
+        return observed
+
+    return _retrieve_events
+
+
+def event_to_json(event):
+    typ = ""
+    obj = {}
+    if isinstance(event, MouseEvent):
+        typ = "MouseEvent"
+        obj = {"x": event.x - 1, "y": event.y - 1}
+        if event.buttons:
+            obj["buttons"] = event.buttons
+
+    if isinstance(event, KeyboardEvent):
+        typ = "KeyboardEvent"
+        obj = {"key": event.key}
+
+        if event.eventType == KeyboardEvent.keyup:
+            obj["eventType"] = "keyup"
+
+    return {"type": typ, "object": obj}
+
+
+@pytest.fixture
+def send_event(emulator_controller):
+    expected = []
+
+    def send_single_event(event, throttle=0.1):
+        time.sleep(throttle)
+
+        if isinstance(event, MouseEvent):
+            emulator_controller.sendMouse(event)
+        if isinstance(event, KeyboardEvent):
+            emulator_controller.sendKey(event)
+
+        expected.append(event_to_json(event))
+        return expected
+
+    return send_single_event
+
+
+@pytest.mark.timeout(timeout=20, func_only=True)
+def test_send_a_sequence_of_single_mouse_events(
+    animation_app, send_event, retrieve_events
+):
+    # Send a series of clicks that we can observe
+    for x in range(150, 160):
+        expected = send_event(MouseEvent(x=x, y=x, buttons=1))
+        expected = send_event(MouseEvent(x=x, y=x, buttons=0))
+
+    assert expected == retrieve_events()
+
+
+@pytest.mark.timeout(timeout=20, func_only=True)
+def test_send_a_sequence_of_single_key_events(
+    animation_app, send_event, retrieve_events
+):
+    # Send a series of clicks that we can observe
+    for key in "abcdefghijklmnopqrstuvwxyz":
+        expected = send_event(KeyboardEvent(key=key, eventType=KeyboardEvent.keydown))
+        expected = send_event(KeyboardEvent(key=key, eventType=KeyboardEvent.keyup))
+
+    assert expected == retrieve_events()
+
+
+@pytest.mark.timeout(timeout=20, func_only=True)
+def test_stream_a_sequence_of_key_events(
+    animation_app, emulator_controller, retrieve_events
+):
+    # Send a series of clicks that we can observe
+    def key_input_event_generator():
+        for key in "abcdefghijklmnopqrstuvwxyz":
+            yield KeyboardEvent(key=key, eventType=KeyboardEvent.keydown)
+            yield KeyboardEvent(key=key, eventType=KeyboardEvent.keyup)
+
+    def delayed_key_input_event_generator():
+        for key in key_input_event_generator():
+            time.sleep(0.1)
+            yield InputEvent(key_event=key)
+
+    emulator_controller.streamInputEvent(delayed_key_input_event_generator())
+
+    expected = [event_to_json(event) for event in key_input_event_generator()]
+    assert expected == retrieve_events()
+
+
+@pytest.mark.timeout(timeout=20, func_only=True)
+def test_stream_a_sequence_of_mouse_events(
+    animation_app, emulator_controller, retrieve_events
+):
+    # Send a series of clicks that we can observe
+    def mouse_input_event_generator():
+        for x in range(150, 160):
+            yield MouseEvent(x=x, y=x, buttons=1)
+            yield MouseEvent(x=x, y=x, buttons=0)
+
+    def delayed_mouse_input_event_generator():
+        for mouse in mouse_input_event_generator():
+            time.sleep(0.1)
+            yield InputEvent(mouse_event=mouse)
+
+    emulator_controller.streamInputEvent(delayed_mouse_input_event_generator())
+
+    expected = [event_to_json(event) for event in mouse_input_event_generator()]
+    assert expected == retrieve_events()
