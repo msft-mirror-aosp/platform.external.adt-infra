c53915f3c5669eb5384e6440f8ca6fb7ad2b4325
Erwin Jansen <jansene@google.com>
2023-07-06 16:46:08 -0700

    Restart adb on adb failures
    We update adb interactions by allowing them to fail one time. If they
    fail we will restart the adb server and attempt again.
    
    Bug: 288974370
    Change-Id: I6128d33bf3195f545a8fe8f8233241754f4abb22

--- pytest/test_embedded/src/emu/emulator.py
+++ pytest/test_embedded/src/emu/emulator.py
@@ -34,7 +34,6 @@ from aemu.proto.emulator_controller_pb2 import (
 )
 from google.protobuf import empty_pb2
 from grpc import RpcError
-from ppadb.client import Client as AdbClient
 
 from emu.adb.adb import Adb
 from emu.avd import AvdWriter
@@ -184,7 +183,7 @@ class BaseEmulator(object):
         Returns:
             bool: True if the emulator has booted, False otherwise.
         """
-        return self.adb.device.wait_boot_complete(timeout=timeout)
+        return self.adb.wait_boot_complete(timeout=timeout)
 
     def console(self) -> EmulatorConnection:
         """Returns a connection to the emulator console, authenticating if needed.
@@ -224,7 +223,7 @@ class BaseEmulator(object):
         Args:
             apk (Path): Path to the apk that should be installed.
             package (str): The name of the package.
-xx
+
         Returns;
             True if the package name is in `pm list packages`
         """
@@ -234,7 +233,7 @@ xx
             time.sleep(1)
             count += 1
 
-        return self.adb.device.is_installed(package_name)
+        return self.adb.is_installed(package_name)
 
     def start_activity(self, activity: str) -> bool:
         """Attempts to start the given activity.
