944f161b9c6c4e219b585438b8f61f456ce7df7f
Joshua Duong <joshuaduong@google.com>
2023-08-25 09:08:38 -0700

    Change warn to panic if no display is detected.
    Immediately fail the build if no display is detected.
    
    Bug: 296110150
    Test: pytest
    Change-Id: I8b2811f76dfb530bde0ec5d270530411215f755f

--- emu_test/utils/common.sh
+++ emu_test/utils/common.sh
@@ -720,7 +720,7 @@ check_physical_display() {
             #           Connection Type: Internal
             display_type=$(system_profiler SPDisplaysDataType | grep "^[[:blank:]]*Displays:")
             if [ -z "$display_type" ]; then
-                warn "No physical display detected. Tests may fail."
+                panic "No physical display detected. Skipping tests and marking build as failed."
             else
                 printf "Display type found.\n"
             fi
