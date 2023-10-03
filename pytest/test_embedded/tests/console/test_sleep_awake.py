1219cbca7d07af004589ac9344de8906ec7d324d
David Yu <dyu@google.com>
2023-10-03 21:32:56 +0000

    Enable fixed test.
    Enable test_power_down_sleeps_the_device fixed on Windows.
    
    BUG: 288441746
    Change-Id: I9d59894f7a33b05591e35ee6bfb5b55051b07d52

--- pytest/test_embedded/tests/console/test_sleep_awake.py
+++ pytest/test_embedded/tests/console/test_sleep_awake.py
@@ -83,7 +83,6 @@ def emulator_off(adb_shell):
 @pytest.mark.adb
 @pytest.mark.timeout(timeout=20, func_only=True)
 @pytest.mark.flaky(reruns=3, reruns_delay=5)
-@pytest.mark.skipos('win', 'b/288441746')
 def test_power_down_sleeps_the_device(adb_shell, emulator_on):
     """Test case to verify that sending the power-down command to an awake device will put the device to sleep."""
 
