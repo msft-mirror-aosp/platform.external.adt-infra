3ab28b9fe655ba815ac3478a3c7cd18685c43c3e
Erwin Jansen <jansene@google.com>
2023-10-09 10:36:22 -0700

    Bind webserver used in test to port in [8000,8100]
    We are seeing test failures on windows when the random parts are in the
    >50000 range. Some stack overflow sleuting points to possible issues
    with virus software etc.
    
    To mitigate this we will find the first available port in a commonly
    used range for development webservers.
    
    Bug: 303719691
    Change-Id: I7c262ca7116f9f8ec1825c2205c9b6148041590f

--- pytest/test_embedded/tests/graphics/test_chrome.py
+++ pytest/test_embedded/tests/graphics/test_chrome.py
@@ -11,14 +11,41 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import http.server
 import threading
+import socket
 
 import pytest
 from aemu.proto.emulator_controller_pb2 import ImageFormat
 
 from emu.timing import wait_until
+
+
+def find_available_port(start_port=8000, num_ports=100):
+    """Find an available port within a specified range.
+
+    This function iterates through a range of ports starting from `start_port` and checks each
+    port to determine if it is available for binding. It returns the first available port found
+    within the specified range.
+
+    Args:
+        start_port (int, optional): The starting port to begin the search (default is 8000).
+        num_ports (int, optional): The number of consecutive ports to check (default is 100).
+
+    Returns:
+        int or None: The first available port found within the specified range, or None if no
+        available port is found.
+    """
+    for port in range(start_port, start_port + num_ports):
+        try:
+            # Attempt to create a socket and bind to the port
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind(("localhost", port))
+            return port
+        except OSError as _err:
+            # Port is already in use, continue to the next one
+            continue
+    return None
 
 
 class TemporaryWebServer(http.server.SimpleHTTPRequestHandler):
@@ -77,8 +104,15 @@ def test_server(request):
             # Access the server using the obtained host and port
             ...
     """
+    # First we find an available port in the range 8000-8100 (b/303719691)
+    port = find_available_port()
+    if port is None:
+        raise OSError(
+            "Unable to find an available port in the range [8000,8100] for binding the webserver."
+        )
+
     # Start the HTTP server in a separate thread
-    server_address = ("", 0)  # Random port
+    server_address = ("", port)
     httpd = http.server.HTTPServer(server_address, TemporaryWebServer)
     thread = threading.Thread(target=httpd.serve_forever)
     thread.daemon = True
@@ -113,15 +147,20 @@ def prepare_chrome(avd):
     avd.adb.shell("am set-debug-app --persistent com.android.chrome")
 
     # Start Chrome for the first time
-    avd.start_activity("com.android.chrome/com.google.android.apps.chrome.Main", params=None)
+    avd.start_activity(
+        "com.android.chrome/com.google.android.apps.chrome.Main", params=None
+    )
 
     # Kill and restart to skip a pop-up
     avd.stop_activity("com.android.chrome")
-    avd.start_activity("com.android.chrome/com.google.android.apps.chrome.Main", params=None)
+    avd.start_activity(
+        "com.android.chrome/com.google.android.apps.chrome.Main", params=None
+    )
 
     yield
 
     avd.stop_activity("com.android.chrome")
+
 
 @pytest.mark.flaky(reruns=3, reruns_delay=5)
 @pytest.mark.graphics
@@ -187,4 +226,6 @@ def test_launch_chrome_google(prepare_chrome, test_server, avd, get_screenshot):
         if wait_until(at_least_40_percent_of_image_is_blue):
             return
 
-    assert False, f"Did not see a screenshot with 40%% blue pixels with {max_retries} retries"
+    assert (
+        False
+    ), f"Did not see a screenshot with 40%% blue pixels with {max_retries} retries"
