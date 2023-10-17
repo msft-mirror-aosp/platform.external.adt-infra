46620da04b07feb6c32e2fcabba97d6cc4186535
Erwin Jansen <jansene@google.com>
2023-10-17 15:08:47 -0700

    Fix incorrect equality operator
    The equality operator was not taking into account that 0 and not present
    are the same in protobuf.
    
    We also increase the tolerance, so we hopefully see fewer issues where
    the sensor value is close, but not exactly the same.
    
    Bug: 306029007
    Change-Id: If72d64f92de0bcc15a939e3fee5eb0df7dcfb6eb

--- pytest/test_embedded/tests/incubating/sensors/test_sensor_service.py
+++ pytest/test_embedded/tests/incubating/sensors/test_sensor_service.py
@@ -21,16 +21,24 @@ from tests.test_utils import StreamingCall
 
 
 def is_equal(model_value, other, consider_equal={}) -> bool:
-    """Returns True if the two PhysicalModelValues are equal, False otherwise."""
+    """
+    Checks whether the two PhysicalModelValues are approximately equal.
 
-    if len(model_value.value.data) != len(other.value.data):
-        return False
+    Args:
+        model_value (PhysicalModelValue): The first PhysicalModelValue for comparison.
+        other (PhysicalModelValue): The second PhysicalModelValue for comparison.
+        consider_equal (dict): Dictionary of values to consider as equal.
 
-    return all(
-        pytest.approx(x) == pytest.approx(y)
-        or (int(x) in consider_equal and consider_equal[int(x)] == pytest.approx(y))
-        for x, y in zip(model_value.value.data, other.value.data)
-    )
+    Returns:
+        bool: True if the two PhysicalModelValues are approximately equal, False otherwise.
+    """
+    for x, y in zip(other.value.data, model_value.value.data):
+        if not (
+            pytest.approx(x, rel=0.1) == y
+            or (int(x) in consider_equal and consider_equal[int(x)] == pytest.approx(y))
+        ):
+            return False
+    return True
 
 
 def set_and_get_sensor(sensor_service, sensor_value):
