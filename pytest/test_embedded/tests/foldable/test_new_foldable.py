9ef614da7179d6dda06dc3c94d5f3db1a74a3632
Bo Hu <bohu@google.com>
2023-11-30 10:48:24 -0800

    foldable: enable the pytest on emutf
    Bug: 314125394
    
    Change-Id: Ie02ac2c033b2f32885e7ec8dfa8c2d83cdef7713

--- pytest/test_embedded/tests/foldable/test_new_foldable.py
+++ pytest/test_embedded/tests/foldable/test_new_foldable.py
@@ -62,7 +62,7 @@ def set_device_hinge_angle(emu, angle):
     )
 
 
-@pytest.mark.foldable
+@pytest.mark.newfoldable
 @pytest.mark.parametrize(
     "fmt,fold_angle,unfold_angle", [(ImageFormat.RGB888, 15.0, 180.0)]
 )
@@ -103,7 +103,7 @@ def test_new_foldable(emulator_controller, fmt, fold_angle, unfold_angle):
     assert image3.format.width > image2.format.width
 
 
-@pytest.mark.foldable
+@pytest.mark.newfoldable
 @pytest.mark.parametrize(
     "fmt,fold_angle,unfold_angle", [(ImageFormat.RGB888, 5.0, 180.0)]
 )
