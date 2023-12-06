ec7476bd12fe0f55999f6f35fed8e3062423127c
David Yu <dyu@google.com>
2023-12-06 21:15:42 +0000

    Add system image UI tests for API 33.
    BUG: 309685259
    Change-Id: I029a9c5e52a401baceeeb1d4cfd8e43e0724b37c

---
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api33/GoogleServicesTest.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.devtools.systemimage.uitest.smoke.api33;
+
+import static org.junit.Assert.assertTrue;
+
+import android.app.Instrumentation;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiScrollable;
+import android.support.test.uiautomator.UiSelector;
+
+import com.android.devtools.systemimage.uitest.annotations.TestInfo;
+import com.android.devtools.systemimage.uitest.common.Res;
+import com.android.devtools.systemimage.uitest.framework.SystemImageTestFramework;
+import com.android.devtools.systemimage.uitest.utils.AppLauncher;
+import com.android.devtools.systemimage.uitest.utils.AppManager;
+import com.android.devtools.systemimage.uitest.utils.GoogleAppUtil;
+import com.android.devtools.systemimage.uitest.watchers.watcher;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test to verify that Google services are available on Google API images
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class GoogleServicesTest {
+    private static final String WIDGET_TEXT_VIEW_CLASS = "android.widget.TextView";
+
+    @Rule
+    public final SystemImageTestFramework testFramework = new SystemImageTestFramework();
+
+    /**
+     * Verifies that Google services are available on Google API images
+     * <p>
+     * This is run to qualify releases. Please involve the test team in substantial changes.
+     * <p>
+     * TR ID: C14578827
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator AVD targeting Google Add On image.
+     *   2. Open Settings > Apps.
+     *   3. From the Overflow menu, select "Show system".
+     *   4. Scroll through the list.
+     *   Verify:
+     *   Google Play Services, Google Services Framework and Maps
+     *   applications are present.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "14578827")
+    public void verifyGoogleApps() throws Exception{
+        Instrumentation instrumentation = testFramework.getInstrumentation();
+
+        if (!testFramework.isGoogleApiImage() || !testFramework.isGoogleApiAndPlayImage()) {
+            return;
+        }
+
+        AppManager.openAppList_v3(instrumentation);
+        AppManager.openSystemAppList_v2(instrumentation);
+
+        UiScrollable appList=
+                new UiScrollable(new UiSelector().resourceIdMatches(Res.SETTINGS_LIST_CONTAINER_RES));
+        appList.setAsVerticalList();
+
+        assertTrue("Cannot find Gmail", appList.getChildByText(
+                new UiSelector().className(WIDGET_TEXT_VIEW_CLASS),
+                "Gmail").exists());
+        assertTrue("Cannot find Google", appList.getChildByText(
+                new UiSelector().className(WIDGET_TEXT_VIEW_CLASS),
+                "Google").exists());
+        assertTrue("Cannot find Google Play services", appList.getChildByText(
+                new UiSelector().className(WIDGET_TEXT_VIEW_CLASS),
+                "Google Play services").exists());
+        assertTrue("Cannot find Google Play Store", appList.getChildByText(
+                new UiSelector().className(WIDGET_TEXT_VIEW_CLASS),
+                "Google Play Store").exists());
+        assertTrue("Cannot find Maps", appList.getChildByText(
+                new UiSelector().className(WIDGET_TEXT_VIEW_CLASS),
+                "Maps").exists());
+    }
+
+    /**
+     * Verify the contents of the Location Settings page
+     * <p>
+     * This is run to qualify releases. Please involve the test team in substantial changes.
+     * <p>
+     * TR ID: C14578827
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator AVD targeting Google Add On image
+     *   2. Open Settings > Location
+     *   Verify:
+     *   Location enable toggle button.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "14578827")
+    public void verifyLocationSettings() throws Exception {
+        Instrumentation instrumentation = testFramework.getInstrumentation();
+        UiDevice device = UiDevice.getInstance(instrumentation);
+
+        // Open settings
+        AppLauncher.launch(instrumentation, "Settings");
+        new watcher(device, Res.ADD_GOOGLE_ACC_WATCHER_PATTERN).checkForCondition();
+
+        //Dismiss Set Up Wizard.
+        UiObject cancelWizard = device.findObject(
+                new UiSelector().resourceIdMatches(Res.CANCEL_SETUP_WIZARD_RES));
+
+        if (cancelWizard.waitForExists(5L)) {
+            cancelWizard.clickAndWaitForNewWindow();
+        }
+
+        //Defer until later today.
+        UiObject deferUntilLater = device.findObject(
+                new UiSelector().resourceIdMatches(Res.DEFERRED_SNOOZE_ITEM_RES).index(0));
+
+        if (deferUntilLater.waitForExists(5L)) {
+            deferUntilLater.clickAndWaitForNewWindow();
+        }
+
+        // Find and click "Location" in Settings
+        UiScrollable itemList =
+                new UiScrollable(
+                        new UiSelector().resourceIdMatches(Res.SETTINGS_LIST_CONTAINER_RES)
+                );
+
+        if (itemList.waitForExists(3L)) {
+            itemList.setAsVerticalList();
+        }
+
+        String securityLabel = "Location";
+        UiObject security = itemList.getChildByText(new UiSelector().className("android.widget.TextView"),
+                securityLabel);
+
+        if (security.waitForExists(3L)) {
+            security.clickAndWaitForNewWindow();
+        }
+
+        assertTrue("Cannot find location toggle button",
+                device.findObject(new UiSelector().resourceIdMatches(Res.ANDROID_SWITCH_TEXT_RES)
+                        .text("Use location")).waitForExists(5L));
+    }
+
+    /**
+     * Logs the user into Google Chrome app.
+     * <p>
+     * This is run to qualify releases. Please involve the test team in substantial changes.
+     * <p>
+     * TR ID: d7f5673a-a3d0-4f50-856a-dfa10ce5c21c
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator with API 24+ Google APIs support.
+     *   2. Launch Chrome.
+     *   3. Log out of Chrome, if logged in already.
+     *   4. Log into Chrome using the test account.
+     *   5. Open the Chrome menu > Settings.
+     *   6. Find logged user name.
+     *   7. Log user out of Chrome.
+     *   Verify:
+     *   1. Logging into Chrome was successful.
+     *   2. Logging out of Chrome was successful.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "d7f5673a-a3d0-4f50-856a-dfa10ce5c21c")
+    public void loginGoogleChrome() throws Exception {
+        Instrumentation instrumentation = testFramework.getInstrumentation();
+
+        GoogleAppUtil.logoutGoogleChrome(instrumentation);
+
+        boolean logInSuccess = GoogleAppUtil.loginGoogleApp(instrumentation, true);
+        assertTrue("Google log in was unsuccessful", logInSuccess);
+
+        boolean logOutSuccess = GoogleAppUtil.logoutGoogleChrome(instrumentation);
+        assertTrue("Google log out was unsuccessful", logOutSuccess);
+    }
+}
---
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api33/PlayStoreTest.java
@@ -0,0 +1,266 @@
+/*
+ * Copyright (c) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.devtools.systemimage.uitest.smoke.api33;
+
+import android.app.Instrumentation;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiSelector;
+
+import com.android.devtools.systemimage.uitest.annotations.TestInfo;
+import com.android.devtools.systemimage.uitest.common.Res;
+import com.android.devtools.systemimage.uitest.framework.SystemImageTestFramework;
+import com.android.devtools.systemimage.uitest.utils.AppLauncher;
+import com.android.devtools.systemimage.uitest.utils.PlayStoreUtil;
+import com.android.devtools.systemimage.uitest.utils.Wait;
+import com.android.devtools.systemimage.uitest.watchers.GoogleAppConfirmationWatcher;
+import com.android.devtools.systemimage.uitest.watchers.watcher;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test to verify that Google services are available on Google API images
+ */
+@RunWith(AndroidJUnit4.class)
+public class PlayStoreTest {
+    @Rule
+    public final SystemImageTestFramework testFramework = new SystemImageTestFramework();
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(600);
+    private final String testApplication = "The Weather Channel";
+    @Before
+    public void verifyPlayStore() throws Exception {
+        if (testFramework.isGoogleApiAndPlayImage()) {
+            Instrumentation instrumentation = testFramework.getInstrumentation();
+            boolean playStoreInstalled = PlayStoreUtil.isPlayStoreInstalled_v2(instrumentation);
+            boolean loggedInToPlayStore = playStoreInstalled &&
+                    PlayStoreUtil.loginGooglePlay(instrumentation);
+            assertTrue("PlayStore login failed.", loggedInToPlayStore);
+        }
+    }
+    /**
+     * Verify apps can be searched through Play Store search bar.
+     * <p>
+     * TT ID: 50027a89-8043-44d7-b7ed-33c631903910
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator and launch home screen.
+     *   2. Open Apps.
+     *   3. Confirm that Play Store is present, then launch.
+     *   4. Search for an app name on the main Play Store screen.
+     *   Verify:
+     *      1. Confirm that app name displays the search string.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "50027a89-8043-44d7-b7ed-33c631903910")
+    public void testPlaySearch() throws Exception {
+        if (testFramework.isGoogleApiAndPlayImage()) {
+            Instrumentation instrumentation = testFramework.getInstrumentation();
+            final UiDevice device = UiDevice.getInstance(instrumentation);
+            assertTrue("Application not found in search.",
+                    PlayStoreUtil.hasTestApp(instrumentation, testApplication));
+            PlayStoreUtil.resetPlayStore(instrumentation);
+            device.pressHome();
+        }
+    }
+    /**
+     * Verify that Google Play can install and uninstall a free app on the device.
+     * <p>
+     * TT ID: cb0ccd97-f045-42fa-8293-a32e94e838aa
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator and launch home screen.
+     *   2. Open Apps drawer.
+     *   3. Confirm that Play Store is present, then launch.
+     *   4. Search for free app in store.
+     *   5. If app is available for install, begin installation.
+     *   6. Uninstall the app.
+     *   Verify:
+     *      1a. If Install button was displayed, allow installation to complete then
+     *      confirm that the Open button to launch the app is present.
+     *      1b. If Install button was not displayed, confirm that the Open button to
+     *      launch the app is present.
+     *      2. Confirm that the app was subsequently uninstalled.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "cb0ccd97-f045-42fa-8293-a32e94e838aa")
+    public void testAppInstallation() throws Exception {
+        if (testFramework.isGoogleApiAndPlayImage()) {
+            Instrumentation instrumentation = testFramework.getInstrumentation();
+            final UiDevice device = UiDevice.getInstance(instrumentation);
+            PlayStoreUtil.selectApplication(instrumentation, testApplication);
+            new GoogleAppConfirmationWatcher(device).checkForCondition();
+            boolean isApplicationInstalled = PlayStoreUtil.installApplication(instrumentation);
+            assertTrue("Unable to install the application from Google Play",
+                    isApplicationInstalled);
+            AppLauncher.launch(instrumentation, "Play Store");
+            assertTrue("Unable to uninstall the application from Google Play",
+                    PlayStoreUtil.uninstallApplication(instrumentation));
+            PlayStoreUtil.resetPlayStore(instrumentation);
+            device.pressHome();
+        }
+    }
+    /**
+     * Verify that Google Play can install and launch an app, then uninstall the app on the device.
+     * <p>
+     * TT ID: 924a0428-4e07-4794-b6a7-2c9d407204aa
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator and launch home screen.
+     *   2. Open Apps.
+     *   3. Confirm that Play Store is present, then launch.
+     *   5. Search for free app in store.
+     *   6. If app is available for install, begin installation.
+     *   7. Launch the application.
+     *   8. Close and uninstall the app.
+     *   Verify:
+     *      1a. If Install button was displayed, allow installation to complete then
+     *      confirm that the Open button to launch the app is present.
+     *      1b. If Install button was not displayed, confirm that the Open button to
+     *      launch the app is present.
+     *      2. Confirm that application launch was successful.
+     *      3. Confirm that the app was subsequently uninstalled.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "924a0428-4e07-4794-b6a7-2c9d407204aa")
+    public void testAppInstallationAndLaunch() throws Exception {
+        if (testFramework.isGoogleApiAndPlayImage()) {
+            Instrumentation instrumentation = testFramework.getInstrumentation();
+            final UiDevice device = UiDevice.getInstance(instrumentation);
+            PlayStoreUtil.selectApplication(instrumentation, testApplication);
+            new GoogleAppConfirmationWatcher(device).checkForCondition();
+            boolean isApplicationInstalled = PlayStoreUtil.installApplication(instrumentation);
+            assertTrue("Unable to install the application from Google Play",
+                    isApplicationInstalled);
+
+            UiObject openButton = device.findObject(new UiSelector()
+                    .className("android.view.View")
+                    .packageName("com.android.vending")
+                    .description("Open"));
+            openButton.clickAndWaitForNewWindow();
+            assertTrue("App could not be opened",
+                    new Wait(TimeUnit.MILLISECONDS.convert(20L, TimeUnit.SECONDS))
+                            .until(() -> device.findObject(new UiSelector()
+                                    .packageName("com.weather.Weather")).exists()));
+
+            AppLauncher.launch(instrumentation, "Play Store");
+            assertTrue("Unable to uninstall the application from Google Play",
+                    PlayStoreUtil.uninstallApplication(instrumentation));
+            PlayStoreUtil.resetPlayStore(instrumentation);
+            device.pressHome();
+        }
+    }
+    /**
+     * Verify that Google Play can reach the payment method prompt during paid app installation
+     * <p>
+     * TT ID: bd9460a8-7b07-4cfc-901f-a99564533e51
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator and launch home screen.
+     *   2. Open Apps.
+     *   3. Confirm that Play Store is present, then launch.
+     *   4. Search for pay app in store.
+     *   Verify:
+     *      1. Confirm that user is presented with a Pay Button with a $.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "bd9460a8-7b07-4cfc-901f-a99564533e51")
+    public void testPayAppVerification() throws Exception {
+        if (testFramework.isGoogleApiAndPlayImage()) {
+            Instrumentation instrumentation = testFramework.getInstrumentation();
+            final UiDevice device = UiDevice.getInstance(instrumentation);
+            final String payApplication = "Bouncer";
+            PlayStoreUtil.selectApplication(instrumentation, payApplication);
+            new GoogleAppConfirmationWatcher(device).checkForCondition();
+            assertTrue("Target application is not a pay app",
+                    device.findObject(new UiSelector()
+                            .className("android.view.View")
+                            .packageName(Res.GOOGLE_PLAY_VENDING_RES)
+                            .descriptionContains("$")).waitForExists(10L));
+            PlayStoreUtil.resetPlayStore(instrumentation);
+            device.pressHome();
+        }
+    }
+    /**
+     * Verify that Google Play search can be limited through parental controls
+     * <p>
+     * TT ID: fe78dba5-a0f2-4acf-bcbb-10b1c15d3484
+     * <p>
+     *   <pre>
+     *   Test Steps:
+     *   1. Start an emulator and launch home screen.
+     *   2. Open Apps.
+     *   3. Confirm that Play Store is present, then launch.
+     *   4. Search for adult app in Play Store, successfully.
+     *   5. Open Play Store settings.
+     *   6. Turn on Parental Controls.
+     *   7. Select restrictions for Apps and Games.
+     *   8. Set and confirm a content PIN.
+     *   9. Set controls to Everyone 10+.
+     *   10. Search for adult app and family app in Play Store, finding only family app.
+     *   11. Turn off Parental Controls.
+     *   Verify:
+     *      1. Confirm that search returns adult version of app without parental controls set.
+     *      2. Confirm that search does not return adult version of app with parental controls set,
+     *          but does return family version of app.
+     *   </pre>
+     */
+    @Test
+    @TestInfo(id = "fe78dba5-a0f2-4acf-bcbb-10b1c15d3484")
+    public void testParentalControls() throws Exception {
+        if (testFramework.isGoogleApiAndPlayImage()) {
+            final Instrumentation instrumentation = testFramework.getInstrumentation();
+            final UiDevice device = UiDevice.getInstance(instrumentation);
+            final String restrictedApplication = "Reddit";
+            assertTrue("Adult application is found in search.",
+                    PlayStoreUtil.hasTestApp(instrumentation, restrictedApplication));
+            device.pressBack();
+
+            new watcher(device, Res.GOOGLE_APP_CONF_WATCHER_PATTERN).checkForCondition();
+
+            PlayStoreUtil.setRestrictions(instrumentation,  "Apps", "Everyone 10+");
+            assertFalse("Adult application is not found in restricted search.",
+                    PlayStoreUtil.hasTestApp(instrumentation, restrictedApplication));
+
+            device.pressBack();
+
+            assertTrue("Family application is found in restricted search.",
+                    PlayStoreUtil.hasTestApp(instrumentation, testApplication));
+
+            device.pressBack();
+            PlayStoreUtil.toggleParentalControls(device, false);
+            device.pressHome();
+        }
+    }
+}
