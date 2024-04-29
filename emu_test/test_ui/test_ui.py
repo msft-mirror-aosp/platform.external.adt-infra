0fe297704b29044b3fa2a3d81efcf1dad9d60691
David Yu <dyu@google.com>
2024-04-29 23:44:55 +0000

    Add more logging info from gradle.
    Check JDK version being used on te build systems for the project.
    
    BUG: b/327455714
    Change-Id: Ib20e2426c0ba61d4d35fbbfab141d5383eb7a7e6

--- emu_test/test_ui/test_ui.py
+++ emu_test/test_ui/test_ui.py
@@ -147,9 +147,9 @@ class UiAutomatorBaseTestCase(EmuBaseTestCase):
         test_ori = test_args_prefix + '.origin=' + avd.ori
         self.m_logger.info('Calling gradle with cwd %r params: %r', self.uitest_dir,
                            [self.gradle, 'cAT', test_class, test_api, test_abi, test_tag,
-                            test_ori, '--stacktrace'])
+                            test_ori, '--stacktrace', '--info'])
         return psutil.Popen([self.gradle, 'cAT', test_class, test_api, test_abi, test_tag,
-                             test_ori, '--stacktrace'], cwd=self.uitest_dir, stdout=PIPE, stderr=PIPE,
+                             test_ori, '--stacktrace', '--info'], cwd=self.uitest_dir, stdout=PIPE, stderr=PIPE,
                             shell=self.use_shell)
 
     def _launch_ui_test_with_avd_configs(self, avd):
