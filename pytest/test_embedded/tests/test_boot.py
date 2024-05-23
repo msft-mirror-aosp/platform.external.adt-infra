f76f012f4fbc395d9d7f389ea751a920955f141b
David Yu <dyu@google.com>
2024-05-23 17:28:00 +0000

    Add ATV test suite for APIs 31, 33 and 34.
    Add P1 scenario for UUID c411f7b5-3802-40c2-90a2-0ce413bc74fa
    Verify that the new emulator build works with TV form factor.
    
    BUG: b/339717700
    Change-Id: I2162bd75bdc0c3b558b33f6e27e4a8c74c9316f7

--- pytest/test_embedded/tests/test_boot.py
+++ pytest/test_embedded/tests/test_boot.py
@@ -98,6 +98,7 @@ async def get_booted_notification_time(emulator):
 @pytest.mark.sanity
 @pytest.mark.fast
 @pytest.mark.wear
+@pytest.mark.atv
 @pytest.mark.async_timeout(1080)
 async def test_first_time_booted(emulator, record_property):
     """Make sure the emulator status is set to booted."""
