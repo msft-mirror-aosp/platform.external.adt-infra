d24b0aa9da455b622fec1215715c8321c6d4460e
Hyun Jae Moon <hyunjaemoon@google.com>
2024-06-04 16:34:38 +0000

    Increase async_timeout to 1080 for netsim tests
    This matches the timeout of test_first_time_booted, in order to
    accomodate longer boot time in Windows.
    
    Test: ./run_tests.sh -e <path/to/emulator> --test_config \
        $REPO/external/adt-infra/pytest/test_embedded/cfg/netsim_tests.json
    Bug: 341386363
    Change-Id: I931c9c2516367e05721ac15d350140cadaa03e67

--- pytest/test_embedded/tests/netsim/test_handle_multi_devices.py
+++ pytest/test_embedded/tests/netsim/test_handle_multi_devices.py
@@ -6,7 +6,7 @@ from netsim_grpc import netsim_client
 # @pytest.mark.boot
 @pytest.mark.netsim
 @pytest.mark.multi
-@pytest.mark.async_timeout(200)
+@pytest.mark.async_timeout(1080)
 def test_two_devices_attach_to_netsimd(avds):
     """Test case to verify that a device is attached to netsimd."""
     assert len(netsim_client.NetsimClient().get_devices()) == 2
