7dc0a0ce3fe5d9d2c85ae0adb41b35694f46f0b9
David Yu <dyu@google.com>
2024-06-18 19:18:24 +0000

    Add P1 stress test for screen capture.
    The test verifies that the screen shot eppears in the specified save
    location. P1 test: 117d44ba-3954-4536-8ace-caa33288af43
    
    Add a tkinter hack to import the package without having to install it
    locally on each machine.
    
    BUG: b/344055568, b/353770597
    Change-Id: I9826ade3846b626ebaa1cef844f3933f5a9d038a

---
+++ pytest/test_embedded/src/hacks/load_tkinter.py
@@ -0,0 +1,22 @@
+# Define a fake internal modeule name to allow tkinter to be found when
+# mouseinfo is called as mouseinfo. This only works if mouseinfo is not use by
+# any other tests. This is needed as tkinter is part of the standard library and
+# tkinter is always imported when pyautogui is requested.
+
+import sys
+import types
+
+try:
+    import tkinter
+except ImportError:
+    # Tkinter may not be available on some Python distributions
+    sys.modules['tkinter'] = types.ModuleType('tkinter')
+    # Make the pymsgbox methods dependent on tkinter unavailable
+    def _couldNotImportPyMsgBox(*unused_args, **unused_kwargs):
+        raise ImportError(
+            "PyAutoGUI was unable to import pymsgbox. " + \
+            "Please install tkinter in order to use this function."
+        )
+    pymsgbox = types.ModuleType('pymsgbox')
+    pymsgbox.alert = pymsgbox.confirm = pymsgbox.prompt = pymsgbox.password = _couldNotImportPyMsgBox
+    sys.modules['pymsgbox'] = pymsgbox
