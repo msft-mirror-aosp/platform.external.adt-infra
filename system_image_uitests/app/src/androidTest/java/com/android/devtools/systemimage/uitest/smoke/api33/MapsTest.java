302e210155a7678185667ce141353f4068fe3a2a
David Yu <dyu@google.com>
2024-07-01 20:08:06 +0000

    Disable testMapsApp on API 33 due to maps app crash.
    The Maps app crashes on API 33 when clicking on the Directions button.
    
    BUG: b/350520274
    Change-Id: I6e2334d2a9ed08681715121cb6db5c007bdafb5e

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api33/MapsTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api33/MapsTest.java
@@ -75,6 +75,7 @@ public class MapsTest {
      */
     @Test
     @TestInfo(id = "4578f63f-7d2e-4e5e-a4e0-0ce2ae67982e")
+    @Ignore("Disabled due to app crash: b/350520274")
     public void testMapsApp() throws Exception {
         Instrumentation instrumentation = testFramework.getInstrumentation();
         UiDevice mDevice = testFramework.getDevice();
