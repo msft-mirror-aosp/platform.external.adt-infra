0cef169cb28e1bf96edd4647a1e9de5c6108fea3
Erwin Jansen <jansene@google.com>
2024-10-24 09:29:41 -0700

    Make the Darwin default system optional
    If we fail to locate the settings plist on darwin we try to fallback to
    the system registry. This is not guaranteed to work, so we make this
    optional.
    
    Change-Id: Idd6c17c3111612da682cf41baca2c282b09eef13

--- pytest/test_embedded/src/emu/qt/qt_settings.py
+++ pytest/test_embedded/src/emu/qt/qt_settings.py
@@ -130,7 +130,7 @@ class QSettings:
         self._array_index: int = -1
         self._status = Status.NoError
         if application:
-            self._domain += f".{application.lower()}"
+            self._domain += f".{application}"
 
         try:
             self._load_settings()
@@ -227,8 +227,7 @@ class QSettings:
                                 f"Invalid defaults data format: {str(e)}"
                             ) from e
                 except subprocess.CalledProcessError as e:
-                    if e.returncode != 1:  # ignore missing defaults
-                        raise AccessError(f"Failed to read defaults: {str(e)}") from e
+                    logging.warning("Failed to read to defaults from system: %s", e)
                     self._data = {}
         except (AccessError, FormatError):
             raise
@@ -319,9 +318,11 @@ class QSettings:
                         text=True,
                     )
                 except subprocess.CalledProcessError as e:
-                    raise SyncError(
-                        f"Failed to write to defaults system: {e.stderr}"
-                    ) from e
+                    logging.warning(
+                        "Failed to write to defaults system: %s, ignoring direct write",
+                        e,
+                    )
+
         except (AccessError, FormatError, SyncError):
             raise
         except Exception as e:
@@ -372,9 +373,10 @@ class QSettings:
                         text=True,
                     )
                 except subprocess.CalledProcessError as e:
-                    raise SyncError(
-                        f"Failed to write to defaults system: {e.stderr}"
-                    ) from e
+                    logging.warning(
+                        "Failed to write to defaults system: %s, ignoring direct write",
+                        e,
+                    )
 
         except SyncError:
             raise
