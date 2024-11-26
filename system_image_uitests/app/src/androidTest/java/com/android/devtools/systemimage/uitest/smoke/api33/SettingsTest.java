bc09efef49d111ce001fbf633ee0b551a1e6571a
David Yu <dyu@google.com>
2024-11-26 18:05:50 +0000

    Launch maps app from the command line in API 33 for testMapPermissions.
    The error log for testMapPermissions indicated that the Maps app was not
    being launched when the test starts. The app will launch from the shell
    command instead of the app.
    
    BUG: b/328433515
    Change-Id: I773d5819f1e9f2e5dd1fcc3e899ff5bf60bb1414

--- system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api33/SettingsTest.java
+++ system_image_uitests/app/src/androidTest/java/com/android/devtools/systemimage/uitest/smoke/api33/SettingsTest.java
@@ -208,26 +208,27 @@ public class SettingsTest {
                 "Deny anyway", "Apps", "Permission manager");
         device.pressHome();
 
-        AppLauncher.launch(instrumentation, appName);
+        device.executeShellCommand("am start -a android.intent.action.VIEW -d geo:0,0");
+        device.waitForIdle();
         final UiObject acceptAndContinueButton;
         acceptAndContinueButton = device.findObject(new UiSelector().
                 textMatches("(?i)accept\\s&\\scontinue"));
-        if (acceptAndContinueButton.exists())
+        if (acceptAndContinueButton.waitForExists(5000L))
             acceptAndContinueButton.clickAndWaitForNewWindow();
 
         final UiObject skipButton;
         skipButton = device.findObject(new UiSelector().textMatches("(?i)skip"));
-        if (skipButton.exists())
+        if (skipButton.waitForExists(5000L))
             skipButton.clickAndWaitForNewWindow();
 
         final UiObject gotItButton;
         gotItButton = device.findObject(new UiSelector().textMatches("(?i)got\\sit"));
-        if (gotItButton.exists())
+        if (gotItButton.waitForExists(5000L))
             gotItButton.clickAndWaitForNewWindow();
 
         final UiObject myLocation;
         myLocation = device.findObject(new UiSelector().resourceId(Res.ANDROID_MY_LOCATION));
-        if (new Wait().until(myLocation::exists))
+        if (myLocation.waitForExists(5000L))
             myLocation.clickAndWaitForNewWindow();
 
         final UiObject allowForegroundButton = device.findObject(
