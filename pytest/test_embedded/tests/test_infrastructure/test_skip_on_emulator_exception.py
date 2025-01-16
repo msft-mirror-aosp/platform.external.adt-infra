501c187054ef6888e388f0dce4f8e7a80be07ff2
Erwin Jansen <jansene@google.com>
2025-01-15 17:02:29 -0800

    test: Intercept fixtures for emulator exceptions
    We now intercept fixtures for emulator failures as well, as this is
    where most of the test failures originate.
    
    Includes a unit test that validates that we intercepts fixtures.
    
    Bug: 390037698
    Change-Id: Icccbcf00f6e44333c9aa60a148898792109f91b6

--- pytest/test_embedded/tests/test_infrastructure/test_skip_on_emulator_exception.py
+++ pytest/test_embedded/tests/test_infrastructure/test_skip_on_emulator_exception.py
@@ -3,6 +3,15 @@ from unittest.mock import patch
 from emu.emulator_exceptions import EmulatorDiedException
 
 
+@pytest.fixture
+def fixture_fail():
+    raise EmulatorDiedException("Simulated emulator death")
+
+
+def test_interceptor_with_fixture_errors(fixture_fail):
+    assert False, "We should not get here"
+
+
 # Dummy test function that simulates an infrastructure error
 def test_interceptor_with_infra_error():
     raise EmulatorDiedException("Simulated emulator death")
