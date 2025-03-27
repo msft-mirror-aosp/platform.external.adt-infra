ad779f27645dabd470ed07661ea22afe734e1762
David Yu <dyu@google.com>
2025-03-27 18:38:00 +0000

    Remove dependency marker for P0 test_close_emulator_with_power_menu.
    This test was disabled as it relies on P0 test_can_open_power_menu. The
    dependent this has now been fixed in b/406533833, so enable this test.
    
    BUG: b/406531392
    Change-Id: Ia85344480f7c9af03457a992e563dcc536ef67a0

--- pytest/test_embedded/tests/events/test_close_emulator.py
+++ pytest/test_embedded/tests/events/test_close_emulator.py
@@ -81,7 +81,6 @@ async def test_can_open_power_menu(open_power_menu, is_power_menu_open):
 
 @pytest.mark.sanity
 @pytest.mark.async_timeout(60)
-@pytest.mark.dependency(depends=["test_can_open_power_menu"])
 async def test_close_emulator_with_power_menu(avd, emulator_is_off, ad_ui):
     """Verifies closing the emulator via the power menu."""
     assert ad_ui(text="Power off").click.wait()
