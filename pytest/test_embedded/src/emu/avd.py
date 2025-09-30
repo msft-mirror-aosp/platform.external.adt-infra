c131f48ae0c5c2ee07a4a76c4b81bf894a86b40c
Kenneth Magic <kmagic@google.com>
2025-09-30 11:05:47 -0700

    Move android-tv api 36 tests to google-tv.
    There is no android-tv api 36 image, but there is a google-tv one.
    Moving to this version to stop the consistent failures.
    
    Bug: 448348896
    Change-Id: I51a63fd86151eaeaaf1a69cc0fcfbea9ea29e655

--- pytest/test_embedded/src/emu/avd.py
+++ pytest/test_embedded/src/emu/avd.py
@@ -550,7 +550,7 @@ class AvdWriter:
         """
         abi = config["abi"]
         tag = config["tag.id"]
-        abi = "x86" if (tag == "android-tv" and abi == "x86_64") else abi
+        abi = "x86" if (tag.endswith("-tv") and abi == "x86_64") else abi
         api = config["api"]
         device_name = config.get("device.name", "Pixel2")
 
