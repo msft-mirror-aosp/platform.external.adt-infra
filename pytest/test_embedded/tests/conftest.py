ef87dbe5c514ca8f82439aebfff0b443fd462707
goog-sergiu <sergiuferentz@google.com>
2026-01-22 16:20:49 +0000

    Add accessibility (color inversion) test
    Bug: 463275389
    Test: ./test_embedded/run_tests.sh -e ~/work/emu/external/qemu/objs/emulator --test_suite accessibility_test_suite_api_36 --local_run
    Change-Id: I172d2f8ac22476a79057c1745f3bcf9e9a72cb85

--- pytest/test_embedded/tests/conftest.py
+++ pytest/test_embedded/tests/conftest.py
@@ -51,6 +51,7 @@ from tests.fixtures.markers import register_markers
 from tests.fixtures.mobly_fixtures import *
 from tests.fixtures.qrcode_fixtures import *
 from tests.fixtures.screen_recording_fixtures import *
+from tests.fixtures.image_fixtures import *
 
 OS_NAME = platform.system().lower()
 HERE = Path(os.path.dirname(__file__)).absolute()
