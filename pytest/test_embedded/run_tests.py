f8000b3e7708904b5f4f35c45fe3e6492dd1706e
Kenneth Magic <kmagic@google.com>
2026-03-10 09:57:06 -0700

    Allow specifying multiple system images for embedded tests.
    The --system_image_path argument now accepts multiple paths, allowing different system images to be used for each AVD in a test configuration. If only one path is provided, it will be applied to all AVDs.
    
    Bug: 490378728
    
    Change-Id: I8a1b4cff324e9e3eec6f48aedd179fca47acd638

--- pytest/test_embedded/run_tests.py
+++ pytest/test_embedded/run_tests.py
@@ -615,6 +615,7 @@ def parse_arguments():
     parser.add_argument(
         "--system_image_path",
         help="Optional path to use for the system image for all tests.",
+        nargs="*",
     )
 
     args = parser.parse_args()
