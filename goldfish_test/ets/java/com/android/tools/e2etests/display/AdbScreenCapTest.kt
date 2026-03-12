a1e40aba19e606cdd0fb44879f610f9d8792716d
Kenneth Magic <kmagic@google.com>
2026-03-12 14:32:25 -0700

    Add the AdbScreencapTest.
    This is a port of test_screen_capture.py and exercises the
    screen cap command via adb.
    
    Bug: 489164205
    Change-Id: Ie6025d9c899b3e9f953913fe38832263d30c6a1b

---
+++ goldfish_test/ets/java/com/android/tools/e2etests/display/AdbScreenCapTest.kt
@@ -0,0 +1,42 @@
+package com.android.tools.e2etests.display
+
+import com.android.tradefed.result.FileInputStreamSource
+import com.android.tradefed.result.LogDataType
+import com.android.tradefed.testtype.DeviceJUnit4ClassRunner
+import com.android.tradefed.testtype.DeviceJUnit4ClassRunner.TestLogData
+import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test
+import java.nio.file.Files
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(DeviceJUnit4ClassRunner::class)
+public class AdbScreenCapTest : BaseHostJUnit4Test() {
+
+  @get:Rule public var mLogs: TestLogData = TestLogData()
+
+  @Test
+  fun screencapCreatesPngFile() {
+    val ts = timestamp()
+    val fileName = "/sdcard/__screenshot_${ts}.png"
+    getDevice().executeShellCommand("rm ${fileName}")
+
+    getDevice().executeShellCommand("screencap ${fileName}")
+    val stdout = getDevice().executeShellCommand("[ -f ${fileName} ] && echo yes")
+    Assert.assertTrue(stdout.contains("yes"))
+
+    val tmpImage = getDevice().pullFile(fileName)
+    mLogs.addTestLog("screencapCreatesPngFile", LogDataType.PNG, FileInputStreamSource(tmpImage))
+
+    val mimeType = Files.probeContentType(tmpImage.toPath())
+    Assert.assertEquals(mimeType, "image/png")
+  }
+}
+
+fun timestamp(): String {
+  val formatter = java.time.format.DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")
+  return java.time.LocalDateTime.now().format(formatter)
+}
