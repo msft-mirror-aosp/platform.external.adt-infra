0f72aaaaf36408915a601b53ee4040308bf967df
Kenneth Magic <kmagic@google.com>
2026-03-26 08:21:53 -0700

    Hide the navigation bar when running.
    The navigation bar can be a source of flakiness as it may dim
    chaging the pixels on the screen.
    
    Bug: 489164205
    Change-Id: Ie7dfc5f89128e603d9561ef5e9ce632ed2b7dfed

--- pytest/test_embedded/AnimateBox/app/src/main/java/com/google/emu/MainActivity.kt
+++ pytest/test_embedded/AnimateBox/app/src/main/java/com/google/emu/MainActivity.kt
@@ -25,7 +25,8 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
         // Hide the status bar.
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         actionBar?.hide()
         // Create a GLSurfaceView instance and set it
         // as the ContentView for this Activity
