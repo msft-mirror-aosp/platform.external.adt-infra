04bc2989ed81e8fd98d0ab6e83ab37baddbcec00
Kenneth Magic <kmagic@google.com>
2026-05-11 13:40:57 -0700

    Add a test to ensure when modules are in the plan files.
    As it is an easy step to miss, the test should ensure the presubmit will fail if a module is added but not include/excluded in the plan files. Note that CallTest and SmsTest were in the situation.
    
    Bug: 511181271
    Change-Id: I5312c02a2e2f8a68ad666d850017e0e303c95179

---
+++ goldfish_test/testlib/ets_modules_test.py
@@ -0,0 +1,67 @@
+"""Tests for the android-ets.zip file.
+
+Tests that all the modules in the android-ets.zip file are explicitly included
+or excluded in the presubmit xml files.
+"""
+
+import io
+import os
+import sys
+import unittest
+from xml.etree import ElementTree
+import zipfile
+
+from python.runfiles import Runfiles
+
+_ETS_ZIP_PATH = ""
+# Modules that are allowed to not be present.
+_ALLOW_LIST = frozenset([
+    "CloseEmulatorTest",  # This is run in a separate tradefed invocation.
+])
+
+
+class EtsModulesTest(unittest.TestCase):
+    def test_ets_modules(self):
+        with zipfile.ZipFile(_ETS_ZIP_PATH, "r") as ets_zip:
+            ets_modules = _get_ets_modules(ets_zip)
+            self.assertNotEqual(0, len(ets_modules))
+            ets_jar = ets_zip.read("android-ets/tools/e2e_tests_post_deploy.jar")
+            next_modules, now_modules = _get_next_and_now_modules(ets_jar)
+        ets_modules = ets_modules - _ALLOW_LIST
+        self.assertCountEqual(ets_modules, next_modules,
+                              "Modules are missing from the presubmit.xml file")
+        self.assertCountEqual(
+            ets_modules, now_modules,
+            "Modules are missing from the emu_now_presubmit.xml file")
+
+
+def _get_next_and_now_modules(ets_jar: bytes) -> tuple[list[str], list[str]]:
+    raw = io.BytesIO(ets_jar)
+    with zipfile.ZipFile(raw, "r") as jar_zip:
+        next_modules = _modules_from_xml(jar_zip.read("config/presubmit.xml"))
+        now_modules = _modules_from_xml(jar_zip.read("config/emu_now_presubmit.xml"))
+    return next_modules, now_modules
+
+
+def _modules_from_xml(xml: bytes) -> list[str]:
+    ret = set()
+    root = ElementTree.fromstring(xml.decode("utf-8"))
+    for e in root:
+        if e.tag == "option":
+          ret.add(e.get("value").split()[0])
+    return ret
+
+
+def _get_ets_modules(ets_zip: zipfile.ZipFile) -> list[str]:
+    ret = set()
+    for name in ets_zip.namelist():
+        if name.endswith(".config"):
+            ret.add(os.path.basename(name)[:-7])
+    return ret
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("Usage: ets_modules_test <ets_zip_path>")
+    _ETS_ZIP_PATH = Runfiles.Create().Rlocation(sys.argv[1])
+    sys.argv = sys.argv[:1]
+    unittest.main()
