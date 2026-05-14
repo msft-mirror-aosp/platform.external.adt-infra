10f3f24c5453fea0501c04b32aa8039f9723d275
Erwin Jansen <jansene@google.com>
2026-05-14 13:18:03 -0700

    chore(test): Remove unused --test_arg= prefix from tradefed args
    Remove the redundant `--test_arg=` prefix when appending instrumentation
    arguments to tradefed. The system handles the raw argument string
    directly, making this parameter prefix unnecessary.
    
    This enables you to run all the tests against a running emulator using:
    
    bazel test @goldfish_test//ets:external_presubmit
    
    Change-Id: Idf48251586b03984ffd3058a6eec3f38b6fea009

--- goldfish_test/sequence/agent_common.py
+++ goldfish_test/sequence/agent_common.py
@@ -18,7 +18,7 @@ def adb(ns: argparse.Namespace, args: list[str]) -> test_sequencer_pb2.AgentConf
         adb=adb_pb2.ADB(
             args=args,
         ),
-        imports = [
+        imports=[
             test_sequencer_pb2.Import(
                 id="android_home",
                 src="android_home",
@@ -27,7 +27,7 @@ def adb(ns: argparse.Namespace, args: list[str]) -> test_sequencer_pb2.AgentConf
                 id="goldfish",
                 src="serial_number",
             ),
-        ]
+        ],
     )
 
 
@@ -190,14 +190,16 @@ def ets_close(ns: argparse.Namespace) -> test_sequencer_pb2.AgentConfig:
     return ac
 
 
-def ets_external(ns: argparse.Namespace, serial_number: str, grpc_port: str) -> test_sequencer_pb2.AgentConfig:
+def ets_external(
+    ns: argparse.Namespace, serial_number: str, grpc_port: str
+) -> test_sequencer_pb2.AgentConfig:
     ac = ets(ns)
     imps = [i for i in ac.imports if i.id != "goldfish"]
     del ac.imports[:]
     ac.imports.extend(imps)
     ac.tradefed.serial_number.append(serial_number)
     ac.tradefed.args.append(
-        "--test_arg=com.android.tradefed.testtype.AndroidJUnitTest:instrumentation-arg:grpc-port:="
+        "com.android.tradefed.testtype.AndroidJUnitTest:instrumentation-arg:grpc-port:="
         + grpc_port
     )
     return ac
