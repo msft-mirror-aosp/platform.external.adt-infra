db881f749849943967d78f7beeb1bb7eb1d7e40f
Daniel Rosenberg <drosen@google.com>
2026-08-12 12:54:03 -0700

    Remove device dumping in enroll_fingerprint
    These debug prints make it harder to view test logs
    
    Change-Id: I308df0b5caf1b3996c3e5861e7db9ef739a9b36b

--- goldfish_test/xts/verifier/enroll_fingerprint.py
+++ goldfish_test/xts/verifier/enroll_fingerprint.py
@@ -196,14 +196,7 @@ def enroll_fingerprint(finger_id):
             print(f"On enrollment screen (touch {i}). Simulating touch...")
             screenshot(f"enroll_touch_{i}")
 
-            if not getattr(enroll_fingerprint, "help_printed", False):
-                print("Dumping input devices:")
-                print(adb("shell", "getevent", "-il", check=False))
-                enroll_fingerprint.help_printed = True
-
             out = adb("emu", "finger", "touch", str(finger_id), check=False)
-            if i == 4 or i == 5:
-                print(f"adb emu finger touch output: '{out}'")
 
             # If emu finger touch fails, fail the enrollment
             if (
